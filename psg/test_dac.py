ce418175a1204241a91d73f2a6446abfb3963ac2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:07 +0100

    fix

--- psg/test_dac.py
+++ psg/test_dac.py
@@ -10,24 +10,27 @@ class Ramps:
     for i in xrange(samplecount):
       buf[i] = i
 
+def expect(*values):
+  return [v * 2 * Dac.halfpoweramp for v in values]
+
 class TestDac(unittest.TestCase):
 
   def test_works(self):
     v = SimpleBuf().atleast(4)
-    d = Dac(Ramps(), self, 15, 13)
+    d = Dac(Ramps(), self, 15, 13, 1)
     self.value = 15
     d(v, 3)
-    self.assertEqual([0, 1, 2, 0], list(v))
+    self.assertEqual(expect(0, 1, 2, 0), list(v))
     self.value = 13
     d(v, 3)
-    self.assertEqual([0, .5, 1, 0], list(v))
-    d = Dac(Ramps(), self, 31, 27)
+    self.assertEqual(expect(0, .5, 1, 0), list(v))
+    d = Dac(Ramps(), self, 31, 27, 1)
     self.value = 31
     d(v, 3)
-    self.assertEqual([0, 1, 2, 0], list(v))
+    self.assertEqual(expect(0, 1, 2, 0), list(v))
     self.value = 23
     d(v, 3)
-    self.assertEqual([0, .25, .5, 0], list(v))
+    self.assertEqual(expect(0, .25, .5, 0), list(v))
 
 if __name__ == '__main__':
   unittest.main()
