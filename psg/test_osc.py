1c4951e88019e7e9b2f5c3ebee997192ddcd4fd3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:32 +0100

    better

--- psg/test_osc.py
+++ psg/test_osc.py
@@ -3,34 +3,40 @@
 import unittest, lfsr
 from osc import ToneOsc, NoiseOsc
 from nod import Block
-
-class Reg:
-
-  def __init__(self, value):
-    self.value = value
+from reg import Register
 
 class TestToneOsc(unittest.TestCase):
 
   def test_works(self):
-    v = ToneOsc(Reg(3))(Block(96)).tolist()
+    o = ToneOsc(Register(3))
+    v = o(Block(96)).tolist()
     self.assertEqual([1] * 24, v[:24])
     self.assertEqual([0] * 24, v[24:48])
     self.assertEqual([1] * 24, v[48:72])
     self.assertEqual([0] * 24, v[72:])
+    v = o(Block(48)).tolist()
+    self.assertEqual([1] * 24, v[:24])
+    self.assertEqual([0] * 24, v[24:])
 
-  def test_stopping(self):
-    v = ToneOsc(Reg(3))(Block(25)).tolist()
+  def test_resume(self):
+    o = ToneOsc(Register(3))
+    v = o(Block(25)).tolist()
     self.assertEqual([1] * 24, v[:24])
     self.assertEqual([0], v[24:])
+    v = o(Block(24)).tolist()
+    self.assertEqual([0] * 23, v[:23])
+    self.assertEqual([1], v[23:])
 
 class TestNoiseOsc(unittest.TestCase):
 
   def test_works(self):
     n = 100
-    v = NoiseOsc(Reg(3))(Block(48 * n)).tolist()
+    o = NoiseOsc(Register(3))
     u = lfsr.Lfsr(*lfsr.ym2149nzdegrees)
-    for i in xrange(n):
-      self.assertEqual([u()] * 48, v[i * 48:(i + 1) * 48])
+    for _ in xrange(2):
+      v = o(Block(48 * n)).tolist()
+      for i in xrange(n):
+        self.assertEqual([u()] * 48, v[i * 48:(i + 1) * 48])
 
 if __name__ == '__main__':
   unittest.main()
