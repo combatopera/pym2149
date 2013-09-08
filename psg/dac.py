29a8d31f665d0f6a9f5deb4e5cef60a77493b958
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:24 +0100

    use lookup, and add one for envelope

--- psg/dac.py
+++ psg/dac.py
@@ -5,21 +5,19 @@ import numpy as np
 class Dac(Node):
 
   headroom = int(2 ** 31 - 2 ** 30.5) # Very close to 3 dB.
-  maxvol = 15
-  halfvol = 13
 
   def __init__(self, signal, modereg, volreg, env, ampshare):
     Node.__init__(self, np.uint32)
-    self.vol = None
-    self.maxamp = 2 ** 31.5 / ampshare
+    maxamp = 2 ** 31.5 / ampshare
+    def createlookup(maxvol, halfvol):
+      return dict([v, int(2 ** ((v - maxvol) / (maxvol - halfvol)) * maxamp)] for v in xrange(maxvol + 1))
+    self.fixedamps = createlookup(15, 13)
+    self.varamps = createlookup(31, 27)
     self.signal = signal
     self.modereg = modereg
     self.volreg = volreg
     self.env = env
 
   def callimpl(self):
-    if self.volreg.value != self.vol:
-      self.amp = int(2 ** ((self.volreg.value - self.maxvol) / (self.maxvol - self.halfvol)) * self.maxamp)
-      self.vol = self.volreg.value
     self.blockbuf.copybuf(self.signal(self.block))
-    self.blockbuf.scale(self.amp)
+    self.blockbuf.scale(self.fixedamps[self.volreg.value])
