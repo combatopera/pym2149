1dc97934bdcabfb645cb93bb137ec516be9d160d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:03 +0100

    passed-in buf should already be big enough

--- psg/fmt.py
+++ psg/fmt.py
@@ -48,5 +48,5 @@ class FloatFormat:
     self.signal = signal
 
   def __call__(self, buf, samplecount):
-    self.signal(buf.atleast(samplecount), samplecount)
+    self.signal(buf, samplecount)
     buf.xform(0, samplecount, self.scale, self.add)
