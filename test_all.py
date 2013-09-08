38ffcc9a8fe84c2f28a258f85c3804d2039a6caa
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:33 +0100

    labels

--- test_all.py
+++ test_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import unittest, os, subprocess
+import unittest, os, subprocess, sys
 
 class TestAll(unittest.TestCase):
 
@@ -9,8 +9,10 @@ class TestAll(unittest.TestCase):
     d, thisname = os.path.split(__file__)
     for name in os.listdir(d):
       if thisname != name and name.startswith('test_') and name.endswith('.py'):
+        print >> sys.stderr, name
         codes.append(subprocess.call([os.path.join(d, name)]))
     self.assertTrue(not codes or set([0]) == set(codes))
+    print >> sys.stderr, thisname
 
 if __name__ == '__main__':
   unittest.main()
