c9f5e2ec503384bc2ef6b4b0011d30a426bc2abf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:21 +0100

    add automated version field

--- psg/reg.py
+++ psg/reg.py
@@ -1,4 +1,10 @@
-class Register:
+class Register(object):
 
   def __init__(self, value):
+    self.version = 0
     self.value = value
+
+  def __setattr__(self, name, value):
+    object.__setattr__(self, name, value)
+    if 'value' == name:
+      self.version += 1
