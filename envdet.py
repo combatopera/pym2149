e586c1684211361980020327f4bfb706e913575a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:26 +0100

    comment

--- envdet.py
+++ envdet.py
@@ -8,7 +8,7 @@ class EnvDet:
   def __init__(self, f):
     period = 1 / f
     self.mul = math.exp(-period / self.rc)
-    self.v = 0
+    self.v = 0 # Logically the value one period ago.
 
   def __call__(self, v):
     self.v = max(v, self.mul * self.v)
