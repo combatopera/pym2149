9f26d806885d24c7cf4ae06eaa5ca6b75d474393
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:53 +0100

    support unlimited blocks, clamp after applying carry

--- pym2149/util.py
+++ pym2149/util.py
@@ -1,15 +1,22 @@
 from __future__ import division
 from nod import Block
 
-def blocks(clock, refreshrate, seconds):
-  framecount = int(round(seconds * clock))
+def blocks(clock, refreshrate, secondsornone = None):
+  if secondsornone is None:
+    acceptframeindex = lambda: True
+    clamp = lambda: blocksize
+  else:
+    framecount = int(round(secondsornone * clock))
+    acceptframeindex = lambda: frameindex < framecount
+    clamp = lambda: min(framecount - frameindex, blocksize)
   frameindex = 0
   carry = 0
-  while frameindex < framecount:
-    blocksize = min(framecount - frameindex, clock // refreshrate)
-    carry += clock % refreshrate
-    while carry >= refreshrate: # Probably at most once.
+  while acceptframeindex():
+    blocksize = clock // refreshrate
+    carry += clock % refreshrate # Adjustment strictly less than refreshrate.
+    if carry >= refreshrate: # Shouldn't be True again after the body.
       blocksize += 1
       carry -= refreshrate
+    blocksize = clamp()
     yield Block(blocksize)
     frameindex += blocksize
