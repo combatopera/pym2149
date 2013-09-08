229773432a5eef40f8eca175ae72675fe856192d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:31 +0100

    fix

--- psg/test_dac.py
+++ psg/test_dac.py
@@ -13,22 +13,11 @@ class Ramps(Node):
     for i in xrange(self.block.framecount):
       self.blockbuf.fill(i, i + 1, i)
 
-def expect(*values):
-  return [np.float32(v * 2 * Dac.halfpoweramp) for v in values]
-
 class TestDac(unittest.TestCase):
 
   def test_works(self):
-    d = Dac(Ramps(), self, 15, 13, 1)
-    self.value = 15
-    self.assertEqual(expect(0, 1, 2), d(Block(3)).tolist())
-    self.value = 13
-    self.assertEqual(expect(0, .5, 1), d(Block(3)).tolist())
-    d = Dac(Ramps(), self, 31, 27, 1)
-    self.value = 31
-    self.assertEqual(expect(0, 1, 2), d(Block(3)).tolist())
-    self.value = 23
-    self.assertEqual(expect(0, .25, .5), d(Block(3)).tolist())
+    d = Dac(Ramps(), 1)
+    self.assertEqual([d.leveltoamp[v] for v in xrange(32)], d(Block(32)).tolist())
 
 if __name__ == '__main__':
   unittest.main()
