06824c5b1055204c8bdcf7df84df706195c783e4
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:13 +0100

    fix

--- psg/test_osc.py
+++ psg/test_osc.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import unittest, lfsr
-from osc import *
-from buf import *
+from osc import ToneOsc, NoiseOsc
+from nod import Block
 
 class Reg:
 
@@ -12,32 +12,25 @@ class Reg:
 class TestToneOsc(unittest.TestCase):
 
   def test_works(self):
-    v = MasterBuf(97)
-    v[96] = 100
-    ToneOsc(Reg(3))(v.ensureandcrop(96))
-    self.assertEqual([1] * 24, list(v[:24]))
-    self.assertEqual([0] * 24, list(v[24:48]))
-    self.assertEqual([1] * 24, list(v[48:72]))
-    self.assertEqual([0] * 24, list(v[72:96]))
-    self.assertEqual([100], list(v[96:]))
+    v = ToneOsc(Reg(3))(Block(96)).tolist()
+    self.assertEqual([1] * 24, v[:24])
+    self.assertEqual([0] * 24, v[24:48])
+    self.assertEqual([1] * 24, v[48:72])
+    self.assertEqual([0] * 24, v[72:])
 
   def test_stopping(self):
-    v = MasterBuf(26)
-    v[25] = 100
-    ToneOsc(Reg(3))(v.ensureandcrop(25))
-    self.assertEqual([1] * 24, list(v[:24]))
-    self.assertEqual([0], list(v[24:25]))
-    self.assertEqual([100], list(v[25:]))
+    v = ToneOsc(Reg(3))(Block(25)).tolist()
+    self.assertEqual([1] * 24, v[:24])
+    self.assertEqual([0], v[24:])
 
 class TestNoiseOsc(unittest.TestCase):
 
   def test_works(self):
     n = 100
-    v = MasterBuf(48 * n)
-    NoiseOsc(Reg(3))(v)
+    v = NoiseOsc(Reg(3))(Block(48 * n)).tolist()
     u = lfsr.Lfsr(*lfsr.ym2149nzdegrees)
     for i in xrange(n):
-      self.assertEqual([u()] * 48, list(v[i * 48:(i + 1) * 48]))
+      self.assertEqual([u()] * 48, v[i * 48:(i + 1) * 48])
 
 if __name__ == '__main__':
   unittest.main()
