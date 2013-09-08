9d79796c34a924ad1030a3f19c69430439e87c05
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:57:59 +0100

    constant

--- psg.py
+++ psg.py
@@ -14,6 +14,7 @@ PSG_NOISE_ARRAY = 8192
 psg_noise = tuple(random.randint(0, 1) for i in xrange(PSG_NOISE_ARRAY))
 PSG_CHANNEL_BUF_LENGTH = 8192 * SCREENS_PER_SOUND_VBL
 psg_channels_buf = [0] * (PSG_CHANNEL_BUF_LENGTH + 16)
+ONE_MILLION = 1048576
 
 def singleton(t):
   return t()
