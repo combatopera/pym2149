141f7f4f6c076b513021f329e214833be3d2ecb5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:47 +0100

    explicit naming

--- buf.py
+++ buf.py
@@ -3,10 +3,10 @@ class Buf:
   def __init__(self):
     self.buf = []
 
-  def atleast(self, n):
-    if len(self.buf) < n:
+  def atleast(self, size):
+    if len(self.buf) < size:
       # I think to extend we'd need yet another sequence, so new list is good:
-      self.buf = [None] * n
+      self.buf = [None] * size
     return self.buf
 
   def __getitem__(self, key):
