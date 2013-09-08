f634e8ae15efc2e101efad3cb85c283abce091d9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:17 +0100

    one val per line

--- test.py
+++ test.py
@@ -6,7 +6,8 @@ def main():
   psg.psg_reg[8] = 16
   psg.psg_reg[0] = 255
   psg.psg_write_buffer(0, psg.PSG_CHANNEL_BUF_LENGTH)
-  print psg.psg_channels_buf
+  for v in psg.psg_channels_buf:
+    print v
 
 if '__main__' == __name__:
   main()
