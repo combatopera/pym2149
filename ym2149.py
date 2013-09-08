39e81e7d045aa39dff41ae8901597d403e754311
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:29 +0100

    wiring for envelope

--- ym2149.py
+++ ym2149.py
@@ -1,7 +1,7 @@
 from __future__ import division
 from psg.reg import Register
 from psg.osc import ToneOsc, NoiseOsc, EnvOsc
-from psg.dac import Dac
+from psg.dac import Level, Dac
 from psg.mix import BinMix, Mixer
 
 class YM2149(Mixer):
@@ -9,6 +9,7 @@ class YM2149(Mixer):
   channels = 3
 
   def __init__(self):
+    # Create registers, not quite 1-1 with the chip:
     regtuple = lambda: tuple(Register(0) for _ in xrange(self.channels))
     self.toneperiods = regtuple()
     self.noiseperiod = Register(0)
@@ -18,10 +19,15 @@ class YM2149(Mixer):
     self.levelmodes = regtuple()
     self.envperiod = Register(0)
     self.envshape = Register(0)
+    # Chip-wide signals:
     noise = NoiseOsc(self.noiseperiod)
     env = EnvOsc(self.envperiod, self.envshape)
+    # Digital channels from binary to level in [0, 31]:
+    channels = [ToneOsc(self.toneperiods[i]) for i in xrange(self.channels)]
+    channels = [BinMix(channel, noise, self.toneflags[i], self.noiseflags[i]) for i, channel in enumerate(channels)]
+    channels = [Level(self.levelmodes[i], self.fixedlevels[i], env, channel) for i, channel in enumerate(channels)]
     # FIXME: All nodes should be called even if excluded from the mix.
-    Mixer.__init__(*[self] + [Dac(BinMix(ToneOsc(self.toneperiods[i]), noise, self.toneflags[i], self.noiseflags[i]), self.levelmodes[i], self.fixedlevels[i], env, self.channels) for i in xrange(self.channels)])
+    Mixer.__init__(*[self] + [Dac(channel, self.channels) for i, channel in enumerate(channels)])
 
   def update(self, frame):
     self.noiseperiod.value = frame[6] & 0x1f
