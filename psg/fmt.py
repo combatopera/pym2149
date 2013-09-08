dade91429e76c9a79b0163dc8b3da82fc91e60b6
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:06 +0100

    simpler

--- psg/fmt.py
+++ psg/fmt.py
@@ -1,10 +1,9 @@
 from __future__ import division
-import math
 
 class FloatFormat:
 
   def __init__(self, signal, maxv):
-    amp = 1 / math.sqrt(2)
+    amp = 2 ** -.5
     self.scale = 2 * amp / maxv
     self.add = -amp
     self.signal = signal
