6096155c26f321cca67e52f71dd682d29559458d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:22 +0100

    comment

--- psg/reg.py
+++ psg/reg.py
@@ -2,7 +2,7 @@ class Register(object):
 
   def __init__(self, value):
     self.version = 0
-    self.value = value
+    self.value = value # This will increment the version to 1.
 
   def __setattr__(self, name, value):
     object.__setattr__(self, name, value)
