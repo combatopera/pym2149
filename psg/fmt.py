c0fbd8e9d156e7a2b37493eed031a88026646cea
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:01 +0100

    float format, using any old buf

--- psg/fmt.py
+++ psg/fmt.py
@@ -38,3 +38,15 @@ class S16LEFormat(LEFormat):
 
   def __init__(self, signal, maxv):
     LEFormat.__init__(self, True, 16, signal, maxv)
+
+class FloatFormat:
+
+  def __init__(self, signal, maxv):
+    amp = 1 / math.sqrt(2)
+    self.scale = 2 * amp / maxv
+    self.add = -amp
+    self.signal = signal
+
+  def __call__(self, buf, samplecount):
+    self.signal(buf.atleast(samplecount), samplecount)
+    buf.xform(0, samplecount, self.scale, self.add)
