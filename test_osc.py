f005dad499077a0a336e92b99a7fcdc69b3327c8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:34 +0100

    tests

---
+++ test_osc.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import unittest, lfsr
+from osc import *
+
+class Reg:
+
+  def __init__(self, value):
+    self.value = value
+
+class TestToneOsc(unittest.TestCase):
+
+  def test_works(self):
+    v = [None] * 96
+    ToneOsc(Reg(3))(v, 0, 96)
+    self.assertEqual([1] * 24, v[:24])
+    self.assertEqual([0] * 24, v[24:48])
+    self.assertEqual([1] * 24, v[48:72])
+    self.assertEqual([0] * 24, v[72:])
+
+class TestNoiseOsc(unittest.TestCase):
+
+  def test_works(self):
+    n = 100
+    v = [None] * (24 * n)
+    NoiseOsc(Reg(3))(v, 0, len(v))
+    u = lfsr.Lfsr(*lfsr.ym2149nzdegrees)
+    for i in xrange(n):
+      self.assertEqual([u()] * 24, v[i * 24:(i + 1) * 24])
+
+if __name__ == '__main__':
+  unittest.main()
