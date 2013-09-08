61b1868aac7c57bdae882caaf69706162a3f75c5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:27 +0100

    correct rc order, take signal in ctor

--- envdet.py
+++ envdet.py
@@ -3,13 +3,14 @@ import math
 
 class EnvDet:
 
-  rc = 0.1e-6 * 1e3 # From schematic.
+  rc = 1e3 * 0.1e-6 # From schematic.
 
-  def __init__(self, f):
+  def __init__(self, signal, f):
     period = 1 / f
     self.mul = math.exp(-period / self.rc)
     self.v = 0 # Logically the value one period ago.
+    self.signal = signal
 
-  def __call__(self, v):
-    self.v = max(v, self.mul * self.v)
+  def __call__(self):
+    self.v = max(self.signal(), self.mul * self.v)
     return self.v
