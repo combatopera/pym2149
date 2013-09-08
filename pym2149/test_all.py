4f3afcd4c97525292ba64cb93cc9f8bc52c797f9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 15:00:04 +0100

    run pyflakes

--- pym2149/test_all.py
+++ pym2149/test_all.py
@@ -5,8 +5,9 @@ import unittest, os, subprocess, sys
 class TestAll(unittest.TestCase):
 
   def test_all(self):
-    codes = []
     d, thisname = os.path.split(__file__)
+    subprocess.call(['pyflakes'] + sorted(n for n in os.listdir(d) if n.endswith('.py')))
+    codes = []
     for name in os.listdir(d):
       if thisname != name and name.startswith('test_') and name.endswith('.py'):
         print >> sys.stderr, name
