0316a1fa96ef464eb2ad0c79dec6fe5516f4fceb
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:41 +0100

    upper clamp and undup

--- fmt.py
+++ fmt.py
@@ -2,38 +2,37 @@ from __future__ import division
 from buf import *
 import math, array
 
-class Format:
+class LEFormat:
 
-  def __init__(self, bits, signal, maxv):
+  def __init__(self, signed, bits, signal, maxv):
     self.buf = Buf()
-    amp = (1 << (bits - 1)) / math.sqrt(2)
+    half = 1 << (bits - 1)
+    amp = half / math.sqrt(2)
     self.scale = 2 * amp / maxv
-    self.shift = (1 << (bits - 1)) - amp
+    self.shift = half - amp
     self.octets = (bits + 7) // 8
+    self.umax = (1 << bits) - 1
+    self.subtract = [0, half][signed]
     self.signal = signal
 
   def createoutbuf(self, count):
     return array.array('B', count * self.octets * [0]) # Unsigned chars as in C.
 
-class U8Format(Format):
-
-  def __init__(self, signal, maxv):
-    Format.__init__(self, 8, signal, maxv)
-
   def __call__(self, buf, count):
     self.signal(self.buf.atleast(count), 0, count)
     for i in xrange(count):
-      buf[i] = int(max(0, self.buf[i] * self.scale + self.shift)) # Effectively floor.
+      x = int(max(0, self.buf[i] * self.scale + self.shift)) # Effectively floor.
+      x = min(self.umax, x) - self.subtract # Clamp other side and apply signedness.
+      for b in xrange(self.octets):
+        buf[self.octets * i + b] = x & 0xff
+        x >>= 8
 
-class S16LEFormat(Format):
+class U8Format(LEFormat):
 
   def __init__(self, signal, maxv):
-    Format.__init__(self, 16, signal, maxv)
+    Format.__init__(self, False, 8, signal, maxv)
 
-  def __call__(self, buf, count):
-    self.signal(self.buf.atleast(count), 0, count)
-    for i in xrange(count):
-      x = min(32767, (int(max(0, self.buf[i] * self.scale + self.shift)) - 32768))
-      buf[2 * i] = x & 0xff
-      x >>= 8
-      buf[2 * i + 1] = x & 0xff
+class S16LEFormat(LEFormat):
+
+  def __init__(self, signal, maxv):
+    Format.__init__(self, True, 16, signal, maxv)
