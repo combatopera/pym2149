e5a6046e66057beb95e2c69401f5e0be5abc5bb9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:39 +0100

    change calling convention for fmt

--- fmt.py
+++ fmt.py
@@ -16,25 +16,20 @@ class U8Format(Format):
   def __init__(self, signal, maxv):
     Format.__init__(self, 8, signal, maxv)
 
-  def __call__(self, buf, bufstart, bufstop):
-    n = bufstop - bufstart
-    self.signal(self.buf.atleast(n), 0, n)
-    for i in xrange(n):
-      buf[bufstart + i] = int(max(0, self.buf[i] * self.scale + self.shift)) # Effectively floor.
+  def __call__(self, buf, count):
+    self.signal(self.buf.atleast(count), 0, count)
+    for i in xrange(count):
+      buf[i] = int(max(0, self.buf[i] * self.scale + self.shift)) # Effectively floor.
 
 class S16LEFormat(Format):
 
   def __init__(self, signal, maxv):
     Format.__init__(self, 16, signal, maxv)
 
-  def __call__(self, buf, bufstart, bufstop):
-    n = (bufstop - bufstart) / 2
-    if n != int(n):
-      raise Exception
-    n = int(n)
-    self.signal(self.buf.atleast(n), 0, n)
-    for i in xrange(n):
+  def __call__(self, buf, count):
+    self.signal(self.buf.atleast(count), 0, count)
+    for i in xrange(count):
       x = min(32767, (int(max(0, self.buf[i] * self.scale + self.shift)) - 32768))
-      buf[bufstart + 2 * i] = x & 0xff
+      buf[2 * i] = x & 0xff
       x >>= 8
-      buf[bufstart + 2 * i + 1] = x & 0xff
+      buf[2 * i + 1] = x & 0xff
