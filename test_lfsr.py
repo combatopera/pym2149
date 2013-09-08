c2fa16181219447c5c08e4ffe94db6852250652f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:58:47 +0100

    comment

--- test_lfsr.py
+++ test_lfsr.py
@@ -6,6 +6,7 @@ from lfsr import *
 class TestLfsr(unittest.TestCase):
 
   def test_correctsequence(self):
+    # Subsequence of the real LFSR from Hatari mailing list:
     expected = (0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0)
     actual = tuple(Lfsr(*ym2149nzdegrees))
     self.assertTrue(''.join(map(str, expected)) in ''.join(map(str, actual)))
