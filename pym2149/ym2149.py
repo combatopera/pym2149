d2e4e914c9f71e0971714b41be7b34906aa55110
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 15:00:08 +0100

    use derived regs, do period clamp here, flags are now not inverted

--- pym2149/ym2149.py
+++ pym2149/ym2149.py
@@ -1,23 +1,32 @@
-from reg import Register
+from reg import Reg, DerivedReg
 from osc import ToneOsc, NoiseOsc, EnvOsc
 from dac import Level, Dac
 from mix import BinMix, Mixer
 
-class YM2149(Mixer):
+class Registers:
 
   channels = 3
 
   def __init__(self):
-    # Create registers, not quite 1-1 with the chip:
-    regtuple = lambda: tuple(Register(0) for _ in xrange(self.channels))
-    self.toneperiods = regtuple()
-    self.noiseperiod = Register(0)
-    self.toneflags = regtuple()
-    self.noiseflags = regtuple()
-    self.fixedlevels = regtuple()
-    self.levelmodes = regtuple()
-    self.envperiod = Register(0)
-    self.envshape = Register(0)
+    self.R = tuple(Reg(0) for i in xrange(16))
+    toneperiodxform = lambda fine, rough: max(1, ((rough & 0x0f) << 8) | (fine & 0xff))
+    envperiodxform = lambda fine, rough: max(1, ((rough & 0xff) << 8) | (fine & 0xff))
+    self.toneperiods = tuple(DerivedReg(toneperiodxform, self.R[c * 2], self.R[c * 2 + 1]) for c in xrange(self.channels))
+    self.noiseperiod = DerivedReg(lambda p: max(1, p & 0x1f), self.R[0x6])
+    def flagxform(b):
+      mask = 0x01 << b
+      return lambda x: not (x & mask)
+    self.toneflags = tuple(DerivedReg(flagxform(c), self.R[0x7]) for c in xrange(self.channels))
+    self.noiseflags = tuple(DerivedReg(flagxform(self.channels + c), self.R[0x7]) for c in xrange(self.channels))
+    self.fixedlevels = tuple(DerivedReg(lambda x: x & 0x0f, self.R[0x8 + c]) for c in xrange(self.channels))
+    self.levelmodes = tuple(DerivedReg(lambda x: x & 0x10, self.R[0x8 + c]) for c in xrange(self.channels))
+    self.envperiod = DerivedReg(envperiodxform, self.R[0xB], self.R[0xC])
+    self.envshape = DerivedReg(lambda x: x & 0x0f, self.R[0xD])
+
+class YM2149(Registers, Mixer):
+
+  def __init__(self):
+    Registers.__init__(self)
     # Chip-wide signals:
     noise = NoiseOsc(self.noiseperiod)
     env = EnvOsc(self.envperiod, self.envshape)
@@ -29,13 +38,6 @@ class YM2149(Mixer):
     Mixer.__init__(*[self] + [Dac(channel, self.channels) for i, channel in enumerate(channels)])
 
   def update(self, frame):
-    self.noiseperiod.value = frame[6] & 0x1f
-    self.envperiod.value = frame[11] | (frame[12] << 8)
-    if 255 != frame[13]:
-      self.envshape.value = frame[13] & 0x0f
-    for i in xrange(self.channels):
-      self.toneperiods[i].value = frame[2 * i] | ((frame[2 * i + 1] & 0x0f) << 8)
-      self.toneflags[i].value = frame[7] & (0x01 << i)
-      self.noiseflags[i].value = frame[7] & (0x08 << i)
-      self.fixedlevels[i].value = frame[8 + i] & 0x0f
-      self.levelmodes[i].value = frame[8 + i] & 0x10
+    for i, x in enumerate(frame):
+      if 0xD != i or 255 != x:
+        self.R[i].value = x
