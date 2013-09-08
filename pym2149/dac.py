21f6ef35116e30bb372801f0dfa2cabfa2169709
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:56 +0100

    comments

--- pym2149/dac.py
+++ pym2149/dac.py
@@ -5,7 +5,8 @@ import numpy as np
 class Level(Node):
 
   def __init__(self, modereg, fixedreg, env, signal):
-    Node.__init__(self, Node.commondtype(env, signal)) # XXX: Or just env?
+    # Probably not necessary for dtype of signal to match that of env and this:
+    Node.__init__(self, Node.commondtype(env, signal))
     self.modereg = modereg
     self.fixedreg = fixedreg
     self.env = env
@@ -17,6 +18,7 @@ class Level(Node):
     else:
       # Convert to equivalent 5-bit level, observe 4-bit 0 is 5-bit 1:
       self.blockbuf.fill(self.fixedreg.value * 2 + 1)
+    # TODO: Find out whether tone+noise should result in mostly envelope or mostly zero:
     self.blockbuf.mulbuf(self.signal(self.block))
 
 class Dac(Node):
@@ -25,8 +27,10 @@ class Dac(Node):
   datum = int(2 ** 31 - 2 ** 30.5) # Half power point, very close to 3 dB.
 
   def __init__(self, level, ampshare):
+    # The level dtype must be such that its values can be used as indices.
     Node.__init__(self, self.dtype)
     maxamp = 2 ** 31.5 / ampshare
+    # Lookup of ideal amplitudes, rounded towards zero:
     self.leveltoamp = np.array([self.dtype(2 ** ((v - 31) / 4) * maxamp) for v in xrange(32)])
     self.level = level
 
