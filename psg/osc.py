cc26477db5545926f485a8e203aa270ff9004fae
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-08 14:59:36 +0100

    all tests pass

--- psg/osc.py
+++ psg/osc.py
@@ -7,10 +7,14 @@ class Osc(Node):
     if 0 != (self.scale % steps):
       raise Exception("Number of steps must divide the scale.")
     Node.__init__(self, np.uint8) # Slightly faster than plain old int.
-    self.indexinstep = 0
-    self.stepindex = -1
+    self.reset()
     self.steps = steps
     self.periodreg = periodreg
+
+  def reset(self):
+    self.indexinstep = 0
+    self.stepindex = -1
+    self.periodindex = -1
 
   def callimpl(self):
     oldperiod = True
@@ -18,9 +22,11 @@ class Osc(Node):
     while frameindex < self.block.framecount:
       if not self.indexinstep:
         self.stepindex = (self.stepindex + 1) % self.steps
-        if oldperiod and not self.stepindex:
-          self.stepsize = self.scale / self.steps * max(1, self.periodreg.value)
-          oldperiod = False
+        if not self.stepindex:
+          self.periodindex += 1
+          if oldperiod:
+            self.stepsize = self.scale / self.steps * max(1, self.periodreg.value)
+            oldperiod = False
         self.value = self.stepvalue(self.stepindex)
       n = min(self.block.framecount - frameindex, self.stepsize - self.indexinstep)
       self.blockbuf.fill(frameindex, frameindex + n, self.value)
@@ -55,6 +61,21 @@ class EnvOsc(Osc):
 
   def __init__(self, periodreg, shapereg):
     Osc.__init__(self, 32, periodreg)
+    self.shapeversion = None
+    self.shapereg = shapereg
+
+  def callimpl(self):
+    if self.shapeversion != self.shapereg.version:
+      self.cont = self.shapereg.value & 0x08
+      self.attack = self.shapereg.value & 0x04
+      self.alternate = self.shapereg.value & 0x02
+      self.hold = self.shapereg.value & 0x01
+      self.shapeversion = self.shapereg.version
+      self.reset()
+    Osc.callimpl(self)
 
   def stepvalue(self, stepindex):
-    return stepindex
+    if self.alternate and (self.periodindex & 1):
+      return 31 - stepindex
+    else:
+      return stepindex
