1442268c7f0935f74b571ee53843f5b97a3cb8cd
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-15 15:25:35 +0100

    comments, renames

--- tests.py
+++ tests.py
@@ -14,42 +14,42 @@ tonenote = 1000 # First peak should have this frequency.
 noisenote = 5000 # First trough should have this frequency, authentic by qnoispec.
 sawnote = 600 # First peak should have this frequency.
 trinote = 1300 # First peak should have half this frequency.
-slowtrinote = 2
+slowtrinote = 2 # Frequency and actual period are both 1.
 
 def main():
-  x = YM2149(1)
-  for i in xrange(x.channels):
-    x.toneflags[i].value = False
-    x.noiseflags[i].value = False
-  x.toneperiods[0].value = int(round(clock / (16 * tonenote)))
-  x.noiseperiod.value = int(round(clock / (16 * noisenote)))
-  x.fixedlevels[0].value = 15
+  chip = YM2149(1) # Stretch 1 channel to full range.
+  for c in xrange(chip.channels):
+    chip.toneflags[c].value = False
+    chip.noiseflags[c].value = False
+  chip.toneperiods[0].value = int(round(clock / (16 * tonenote)))
+  chip.noiseperiod.value = int(round(clock / (16 * noisenote)))
+  chip.fixedlevels[0].value = 15
   def dump(path):
     path = os.path.join('target', path)
     print >> sys.stderr, path
-    w = WavWriter(1, clock, x, outfreq, path)
+    w = WavWriter(1, clock, chip, outfreq, path)
     for block in blocks(clock, refreshrate, seconds):
       w(block)
     w.close()
-  x.toneflags[0].value = True
+  chip.toneflags[0].value = True
   dump('1ktone.wav')
-  x.toneflags[0].value = False
-  x.noiseflags[0].value = True
+  chip.toneflags[0].value = False
+  chip.noiseflags[0].value = True
   dump('5knoise.wav')
-  x.toneflags[0].value = True
+  chip.toneflags[0].value = True
   dump('1ktone5knoise.wav')
-  x.toneflags[0].value = False
-  x.noiseflags[0].value = False
-  x.levelmodes[0].value = 1 # Envelope on.
-  x.envperiod.value = int(round(clock / (256 * sawnote)))
-  x.envshape.value = 0x08
+  chip.toneflags[0].value = False
+  chip.noiseflags[0].value = False
+  chip.levelmodes[0].value = 1 # Envelope on.
+  chip.envperiod.value = int(round(clock / (256 * sawnote)))
+  chip.envshape.value = 0x08
   dump('600saw.wav')
-  x.envperiod.value = int(round(clock / (256 * trinote)))
-  x.envshape.value = 0x0a
+  chip.envperiod.value = int(round(clock / (256 * trinote)))
+  chip.envshape.value = 0x0a
   dump('650tri.wav')
-  x.envperiod.value = int(round(clock / (256 * slowtrinote)))
-  x.toneflags[0].value = True
-  x.noiseflags[0].value = True
+  chip.envperiod.value = int(round(clock / (256 * slowtrinote)))
+  chip.toneflags[0].value = True
+  chip.noiseflags[0].value = True
   dump('1tri1ktone5knoise.wav')
 
 if '__main__' == __name__:
