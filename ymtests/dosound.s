fba3b94fd484cdafe5c3ec49ad507e71259439af
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-09-21 20:37:42 +0100

    also test going up forever

--- ymtests/dosound.s
+++ ymtests/dosound.s
@@ -1,25 +1,32 @@
-	pea	script
+	bsr	vsync
+	lea	scripts,a6
+
+next:	move.l	(a6)+,-(sp)
 	move	#32,-(sp)
 	trap	#14
 	addq.l	#6,sp
 
 	moveq	#49,d7
+wait:	bsr	vsync
+	dbra	d7,wait
+
+	tst.l	(a6)
+	bne	next
+
+	clr	-(sp)
+	trap	#1
 
 vsync:	move	#37,-(sp)
 	trap	#14
 	addq.l	#2,sp
-
-	dbra	d7,vsync
-
-	clr	-(sp)
-	trap	#1
+	rts
 
 script:	dc.b	$7,$ff,$8,$00,$9,$00,$A,$00
 	dc.b	$82,1
 	dc.b	$8,$0d
 	dc.b	$82,1
 	dc.b	$8,$00
-	dc.b	$8,$0d
+	dc.b	$8,$0f
 	dc.b	$8,$00
 	dc.b	$82,2
 	dc.b	$8,$0d
@@ -28,5 +35,11 @@ script:	dc.b	$7,$ff,$8,$00,$9,$00,$A,$00
 	dc.b	$81,$8,-1,$0d
 	dc.b	$80,$10
 	dc.b	$81,$8,-2,$0d
-	dc.b	$8,$0f
-	dc.b	$82,0
+
+script2:	dc.b	$7,$ff,$8,$00,$9,$00,$A,$00
+	dc.b	$80,$ff
+	dc.b	$81,$8,1,$0d
+	dc.b	$80,$fe
+	dc.b	$81,$8,2,$0d
+
+scripts:	dc.l	script,script2,0
