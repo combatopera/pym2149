ee2915662a77c0b739a89b933aa3ba1a1cd916a2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-09 08:13:03 +0100

    always deflect from a fixed reference, maybe this will actually be useful

--- ymtests/qanlgmix.py
+++ ymtests/qanlgmix.py
@@ -1,8 +1,15 @@
 mixer.put() # All off.
+def reset():
+  for l in A_level, B_level, C_level:
+    l.put(0x0d) # Half amplitude.
+reset()
+sleep(50) # Allow it to settle at DC 0.
 for a in xrange(0x10):
-  A_level.put(a)
   for b in xrange(0x10):
-    B_level.put(b)
     for c in xrange(0x10):
+      A_level.put(a)
+      B_level.put(b)
       C_level.put(c)
+      sleep(2)
+      reset()
       sleep(2)
