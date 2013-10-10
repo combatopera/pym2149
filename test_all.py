7fb96c0db844279ddcc60bb1cbe30b70e0313d1f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-10 08:20:11 +0100

    run all tests in project

---
+++ test_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import unittest, os, subprocess, sys
+
+class TestAll(unittest.TestCase):
+
+  def test_all(self):
+    thispath = __file__
+    top, thisname = os.path.split(thispath)
+    paths = []
+    for dirpath, dirnames, filenames in os.walk(top):
+      paths.extend(sorted(os.path.join(dirpath, n) for n in filenames if n.endswith('.py')))
+      dirnames.sort()
+    subprocess.call(['pyflakes'] + paths)
+    codes = []
+    for path in paths:
+      name = os.path.basename(path)
+      if thisname != name and name.startswith('test_'):
+        print >> sys.stderr, path
+        codes.append(subprocess.call(path))
+    self.assertTrue(not codes or set([0]) == set(codes))
+    print >> sys.stderr, thispath
+
+if __name__ == '__main__':
+  unittest.main()
