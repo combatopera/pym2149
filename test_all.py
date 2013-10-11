6426f2b1658a5e1bef253b9a87d0f301c5b6186f
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-11 08:21:18 +0100

    exempt ym tests from pyflakes

--- test_all.py
+++ test_all.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
-import unittest, os, subprocess, sys
+import unittest, os, subprocess, sys, re
 
 class TestAll(unittest.TestCase):
 
   def test_all(self):
     thispath = __file__
     top, thisname = os.path.split(thispath)
+    flakesregex = []
+    f = open(os.path.join(top, '.flakesignore'))
+    try:
+      for line in f:
+        line, = line.splitlines()
+        flakesregex.append(line)
+    finally:
+      f.close()
+    flakesregex = re.compile('|'.join(flakesregex))
+    def flakesaccept(path):
+      return flakesregex.search(path[len(top) + len(os.sep):].replace(os.sep, '/')) is None
     paths = []
     for dirpath, dirnames, filenames in os.walk(top):
       paths.extend(sorted(os.path.join(dirpath, n) for n in filenames if n.endswith('.py')))
       dirnames.sort()
-    subprocess.call(['pyflakes'] + paths)
+    subprocess.call(['pyflakes'] + [p for p in paths if flakesaccept(p)])
     codes = []
     for path in paths:
       name = os.path.basename(path)
