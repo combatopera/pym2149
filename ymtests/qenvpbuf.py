fc3c4443202dcc8dbdb538402e298d9c12c11125
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-14 08:20:58 +0100

    add answer

--- ymtests/qenvpbuf.py
+++ ymtests/qenvpbuf.py
@@ -1,10 +1,10 @@
 mixer.put() # All off.
 A_level.put(0x10) # Variable level.
 E_shape.put(0x0a) # Triangle.
-E_fine.put(0x77) # Non-zero.
-for _ in xrange(10):
-  E_rough.put(0xff)
-  sleep(25)
-  E_rough.put(0x00)
-  sleep(25)
+E_fine.put(0x00)
+for i in xrange(20):
+  E_rough.put(0x05)
+  sleep(2 + i)
+  E_rough.put(0x02)
+  sleep(2 + i)
 A_level.put(0x00)
