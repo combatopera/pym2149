600af17793569ddc2c7d0ac33ff7ac3ac02b70a5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-22 18:14:16 +0100

    logging

--- dosound2wav.py
+++ dosound2wav.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from __future__ import division
-import sys, re
+import sys, re, logging
 from pym2149.dosound import dosound
 from pym2149.ym2149 import YM2149, stclock as clock
 from pym2149.out import WavWriter
-from pym2149.util import Session
+from pym2149.util import Session, initlogging
+
+log = logging.getLogger(__name__)
 
 extraseconds = 3
 pattern = re.compile(r'^([^\s]+)?\s+([^\s]+)?\s+([^\s]+)?')
@@ -44,6 +46,7 @@ def readbytecode(f, label):
   return bytecode
 
 def main():
+  initlogging()
   inpath, label, outpath = sys.argv[1:]
   f = open(inpath)
   try:
@@ -55,6 +58,7 @@ def main():
   try:
     session = Session(clock)
     dosound(bytecode, chip, session, stream)
+    log.info("Streaming %.3f extra seconds.", extraseconds)
     for b in session.blocks(1 / extraseconds):
       stream(b)
     stream.flush()
