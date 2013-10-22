e0034cef3ef97e75c1d4027de116aac5ebdb5833
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-22 18:28:17 +0100

    logging

--- dsd2wav.py
+++ dsd2wav.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
-import sys
+import sys, logging
 from pym2149.dosound import dosound
 from pym2149.ym2149 import YM2149, stclock as clock
 from pym2149.out import WavWriter
-from pym2149.util import Session
+from pym2149.util import Session, initlogging
+
+log = logging.getLogger(__name__)
 
 def main():
+  initlogging()
   inpath, outpath = sys.argv[1:]
   f = open(inpath, 'rb')
   try:
-    f.read(2) # Skip total ticks.
+    log.debug("Total ticks: %s", (ord(f.read(1)) << 8) | ord(f.read(1)))
     bytecode = [ord(c) for c in f.read()]
   finally:
     f.close()
