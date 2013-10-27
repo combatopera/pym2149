d71132567962263000df946af211a23de60389d8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-10-27 20:02:03 +0000

    chip scale can now be passed in

--- ym2wav.py
+++ ym2wav.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python
 
-import sys, logging
+from __future__ import division
+import sys, logging, getopt
 from pym2149.out import WavWriter
 from pym2149.util import Session, initlogging
-from pym2149.ym2149 import YM2149
+from pym2149.ym2149 import YM2149, defaultscale
 from pym2149.ymformat import ymopen
 from pym2149.mix import Mixer
 
 log = logging.getLogger(__name__)
 
+def uniqueoption(options, keys, defaultval, xform):
+  vals = [v for k, v in options if k in keys]
+  if not vals:
+    return defaultval
+  v, = vals
+  return xform(v)
+
 def main():
   initlogging()
-  inpath, outpath = sys.argv[1:]
+  options, args = getopt.getopt(sys.argv[1:], 's:', ['scale='])
+  scale = uniqueoption(options, ('-s', '--scale'), defaultscale, int)
+  inpath, outpath = args
   f = ymopen(inpath)
   try:
     for info in f.info:
       log.info(info)
-    chip = YM2149()
-    stream = WavWriter(f.clock, Mixer(*chip.dacs), outpath)
+    chip = YM2149(scale = scale)
+    clock = f.clock * scale / 8 # Observe may be non-integer.
+    stream = WavWriter(clock, Mixer(*chip.dacs), outpath)
     try:
-      session = Session(f.clock)
+      session = Session(clock)
       for frame in f:
         chip.update(frame)
         for b in session.blocks(f.framefreq):
