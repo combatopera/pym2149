50c317cee66a8a652d37d98fbee529b3e611189d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2013-12-29 11:09:02 +0000

    handy

--- pym2149/env.py
+++ pym2149/env.py
@@ -6,13 +6,17 @@ class Env(list):
     self.append(initial)
 
   def lin(self, n, target):
-    if n:
-      source = self[-1]
-      for i in xrange(1, n + 1):
-        self.append(int(round(source + (target - source) * i / n)))
-    else:
-      self[-1] = target
+    source = self[-1]
+    for i in xrange(1, n + 1):
+      self.append(int(round(source + (target - source) * i / n)))
     return self
+
+  def jump(self, target):
+    self[-1] = target
+    return self
+
+  def hold(self, n):
+    return self.lin(n, self[-1])
 
   def __call__(self, frame):
     return self[min(frame, len(self) - 1)]
--- pym2149/test_env.py
+++ pym2149/test_env.py
@@ -7,7 +7,7 @@ class TestEnv(unittest.TestCase):
 
   def test_works(self):
     self.assertEqual([5], Env(5))
-    self.assertEqual([8], Env(5).lin(0, 8)) # Replace.
+    self.assertEqual([8], Env(5).jump(8))
     self.assertEqual([5, 8], Env(5).lin(1, 8))
     self.assertEqual([5, 6, 7, 8], Env(5).lin(3, 8))
     self.assertEqual([5, 7, 8], Env(5).lin(2, 8))
@@ -18,8 +18,8 @@ class TestEnv(unittest.TestCase):
     self.assertEqual([-5, -7, -8], Env(-5).lin(2, -8))
     self.assertEqual([-5, -6, -7, -7, -8], Env(-5).lin(4, -8))
     # Holds:
-    self.assertEqual([5, 5, 5, 5, 6, 7, 8], Env(5).lin(3, 5).lin(3, 8))
-    self.assertEqual([5, 5, 5, 8], Env(5).lin(3, 5).lin(0, 8))
+    self.assertEqual([5, 5, 5, 5, 6, 7, 8], Env(5).hold(3).lin(3, 8))
+    self.assertEqual([5, 5, 5, 8], Env(5).hold(3).jump(8))
 
 if __name__ == '__main__':
   unittest.main()
