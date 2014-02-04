faae88bb9115a052cdc8dd1aaac20eab936b3359
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-02-04 21:50:00 +0000

    demo square wave made from minbleps

--- minblep.py
+++ minblep.py
@@ -23,11 +23,23 @@ class MinBlep:
     realcepstrum[1:self.midpoint + 1] *= 2
     realcepstrum[self.midpoint + 1:] = 0
     self.minbli = np.fft.ifft(np.exp(np.fft.fft(realcepstrum))).real
-    # Check our cepstrum manipulation isn't broken:
+    # Check our cepstrum manipulation isn't broken. TODO: Replace with unit test.
     if not np.allclose(absdft, np.abs(np.fft.fft(self.minbli))):
       log.warn('Bad min-phase reconstruction.')
     self.minblep = np.cumsum(self.minbli)
     self.scale = scale
+
+  def getmixin(self, ctrlx, ctrlrate, outrate, amp, buf):
+    import math
+    outx = ctrlx / ctrlrate * outrate # 0, 0+, .5, 1-, 1
+    outi = int(math.ceil(outx)) # 0, 1, 1, 1, 1
+    frac = outi - outx # 0, 1-, .5, 0+, 0
+    shape = int(round(frac * self.scale)) # XXX: Is round correct here?
+    view = self.minblep[shape::self.scale] # Two possible sizes.
+    buf = buf.ensureandcrop(len(view))
+    buf.buf[:] = view
+    buf.buf *= amp
+    return outi, buf
 
 def plot():
   import matplotlib.pyplot as plt
@@ -40,5 +52,45 @@ def plot():
   plt.grid(True)
   plt.show()
 
+def render():
+  import subprocess, sys, fractions
+  from pym2149.buf import MasterBuf
+  ctrlrate = 2000000
+  outrate = 44100
+  tonefreq = 1500
+  toneamp = .25
+  idealscale = ctrlrate // fractions.gcd(ctrlrate, outrate)
+  scale = 1000 # TODO: Find out what we can get away with.
+  ctrlsize = ctrlrate
+  outsize = outrate
+  dtype = np.float32
+  toneoscscale = 16
+  periodreg = int(round(ctrlrate / (toneoscscale * tonefreq)))
+  period = toneoscscale * periodreg
+  ctrlsignal = np.empty(ctrlsize, dtype = dtype)
+  diffsignal = np.empty(ctrlsize, dtype = dtype)
+  outsignal = np.empty(outsize, dtype = dtype)
+  minblepbuf = MasterBuf(dtype = dtype)
+  x = 0
+  while x < ctrlsize:
+    ctrlsignal[x:x + period // 2] = toneamp
+    ctrlsignal[x + period // 2:x + period] = -toneamp
+    x += period
+  minblep = MinBlep(40, scale)
+  diffsignal[:] = ctrlsignal
+  diffsignal[0] -= 0 # Last value of previous ctrlsignal.
+  diffsignal[1:] -= ctrlsignal[:-1]
+  outsignal[:] = 0
+  for ctrlx in np.flatnonzero(diffsignal): # XXX: Can we avoid making a new array?
+    outi, mixin = minblep.getmixin(ctrlx, ctrlrate, outrate, diffsignal[ctrlx], minblepbuf)
+    outj = min(outsize, outi + len(mixin.buf))
+    outsignal[outi:outj] += mixin.buf[:outj - outi]
+    outsignal[outj:] += diffsignal[ctrlx]
+  command = ['sox', '-t', 'raw', '-r', str(outrate), '-e', 'float', '-b', '32', '-', 'minblep.wav']
+  sox = subprocess.Popen(command, stdin = subprocess.PIPE)
+  outsignal.tofile(sox.stdin)
+  sox.stdin.close()
+  sys.exit(sox.wait())
+
 if __name__ == '__main__':
-  plot()
+  render()
