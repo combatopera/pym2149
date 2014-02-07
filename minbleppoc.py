8f923ae4c61f063afda3d301702d847c2e86b42b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-02-07 20:21:03 +0000

    rename var

--- minbleppoc.py
+++ minbleppoc.py
@@ -7,39 +7,39 @@ from pym2149.minblep import MinBleps
 
 def main():
   import subprocess, sys
-  ctrlrate = 2000000
+  naiverate = 2000000
   outrate = 44100
   tonefreq = 1500
   toneamp = .25
   scale = 500 # Smaller values result in worse-looking spectrograms.
-  ctrlsize = ctrlrate
+  naivesize = naiverate
   outsize = outrate
   dtype = np.float32
   toneoscscale = 16
-  periodreg = int(round(ctrlrate / (toneoscscale * tonefreq)))
+  periodreg = int(round(naiverate / (toneoscscale * tonefreq)))
   period = toneoscscale * periodreg
-  ctrlmaster = MasterBuf(dtype = dtype)
+  naivemaster = MasterBuf(dtype = dtype)
   diffmaster = MasterBuf(dtype = dtype)
   outmaster = MasterBuf(dtype = dtype)
   minblepbuf = MasterBuf(dtype = dtype)
-  ctrlbuf = ctrlmaster.ensureandcrop(ctrlsize)
+  naivebuf = naivemaster.ensureandcrop(naivesize)
   x = 0
-  while x < ctrlsize:
-    ctrlbuf.fillpart(x, x + period // 2, toneamp)
-    ctrlbuf.fillpart(x + period // 2, x + period, -toneamp)
+  while x < naivesize:
+    naivebuf.fillpart(x, x + period // 2, toneamp)
+    naivebuf.fillpart(x + period // 2, x + period, -toneamp)
     x += period
   minbleps = MinBleps(scale)
-  diffbuf = diffmaster.ensureandcrop(ctrlsize)
-  diffbuf.copybuf(ctrlbuf)
-  diffbuf.buf[0] -= 0 # Last value of previous ctrlsignal.
-  diffbuf.buf[1:] -= ctrlbuf.buf[:-1]
+  diffbuf = diffmaster.ensureandcrop(naivesize)
+  diffbuf.copybuf(naivebuf)
+  diffbuf.buf[0] -= 0 # Last value of previous naivesignal.
+  diffbuf.buf[1:] -= naivebuf.buf[:-1]
   outbuf = outmaster.ensureandcrop(outsize)
   outbuf.fill(0)
-  for ctrlx in np.flatnonzero(diffbuf.buf): # XXX: Can we avoid making a new array?
-    outi, mixin = minbleps.getmixin(ctrlx, ctrlrate, outrate, diffbuf.buf[ctrlx], minblepbuf)
+  for naivex in np.flatnonzero(diffbuf.buf): # XXX: Can we avoid making a new array?
+    outi, mixin = minbleps.getmixin(naivex, naiverate, outrate, diffbuf.buf[naivex], minblepbuf)
     outj = min(outsize, outi + len(mixin.buf))
     outbuf.buf[outi:outj] += mixin.buf[:outj - outi]
-    outbuf.buf[outj:] += diffbuf.buf[ctrlx]
+    outbuf.buf[outj:] += diffbuf.buf[naivex]
   command = ['sox', '-t', 'raw', '-r', str(outrate), '-e', 'float', '-b', '32', '-', 'minbleppoc.wav']
   sox = subprocess.Popen(command, stdin = subprocess.PIPE)
   outbuf.tofile(sox.stdin)
