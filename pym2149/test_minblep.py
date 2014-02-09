edfd1c70a2d70ee884e5896c3a190c18336c4e23
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-02-09 16:34:20 +0000

    tweak

--- pym2149/test_minblep.py
+++ pym2149/test_minblep.py
@@ -6,7 +6,7 @@ from minblep import MinBleps
 class TestMinBleps(unittest.TestCase):
 
   def test_minphasereconstruction(self):
-    minbleps = MinBleps(12, 1, 500)
+    minbleps = MinBleps(1, 1, 500)
     absdft = np.abs(np.fft.fft(minbleps.bli))
     absdft2 = np.abs(np.fft.fft(minbleps.minbli))
     self.assertTrue(np.allclose(absdft, absdft2))
