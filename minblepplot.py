9f736808eb7aa80a966d26cbdea68946142964a8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-02-15 10:21:51 +0000

    allegedly fft is faster when size is a power of 2, so let's do that. now the cepstrum has an even length.

--- minblepplot.py
+++ minblepplot.py
@@ -4,14 +4,23 @@ from __future__ import division
 import numpy as np
 from pym2149.minblep import MinBleps
 
+def plot(plt, mb, signal, style, padstyle):
+  plt.plot(np.arange(mb.lpad), signal[:mb.lpad], padstyle)
+  plt.plot(mb.lpad + np.arange(mb.kernelsize), signal[mb.lpad:-mb.rpad], style)
+  plt.plot(mb.lpad + mb.kernelsize + np.arange(mb.rpad), signal[-mb.rpad:], padstyle)
+
+def plotmin(plt, mb, shift, signal, style):
+  plt.plot(mb.midpoint + shift + np.arange(len(signal)), signal, style)
+
 def main():
   import matplotlib.pyplot as plt
-  mb = MinBleps(1, 1, 5, cutoff = .5, transition = 4 / 20)
-  plt.plot(mb.bli * mb.scale, 'b+')
-  plt.plot(mb.blep, 'bo')
-  plt.plot(np.arange(mb.size) + mb.midpoint, mb.minbli * mb.scale, 'r+')
-  for style in 'r', 'ro':
-    plt.plot(np.arange(mb.size + mb.scale - 1) + mb.midpoint, mb.minblep, style)
+  mb = MinBleps(1, 1, 5, cutoff = .5, transition = 4 / 10)
+  plot(plt, mb, mb.bli * mb.scale, 'b+', 'c+')
+  plot(plt, mb, mb.blep, 'bo', 'co')
+  plotmin(plt, mb, 0, mb.minbli * mb.scale, 'r+')
+  plotmin(plt, mb, 0, mb.minblep, 'r')
+  plotmin(plt, mb, 0, mb.minblep[:mb.size], 'ro')
+  plotmin(plt, mb, mb.size, mb.minblep[mb.size:], 'yo')
   plt.grid(True)
   plt.show()
 
--- pym2149/minblep.py
+++ pym2149/minblep.py
@@ -6,29 +6,40 @@ class MinBleps:
   minmag = np.exp(-100)
 
   def __init__(self, ctrlrate, outrate, scale, cutoff = .475, transition = .05):
+    # XXX: Use kaiser and/or satisfy min transition?
     # Closest even order to 4/transition:
     order = int(round(4 / transition / 2)) * 2
-    self.midpoint = order * scale // 2 # Index of peak of sinc.
-    self.size = order * scale + 1
-    x = (np.arange(self.size) / (self.size - 1) * 2 - 1) * order * cutoff
+    self.kernelsize = order * scale + 1
+    self.size = 2 ** 0
+    while self.size < self.kernelsize:
+      self.size <<= 1
+    self.midpoint = self.size // 2 # Index of peak of sinc.
+    x = (np.arange(self.kernelsize) / (self.kernelsize - 1) * 2 - 1) * order * cutoff
     # If cutoff is .5 the sinc starts and ends with zero.
     # The window is necessary for a reliable integral height later:
-    self.bli = np.blackman(self.size) * np.sinc(x) / scale * cutoff * 2
+    self.bli = np.blackman(self.kernelsize) * np.sinc(x) / scale * cutoff * 2
+    self.rpad = (self.size - self.kernelsize) // 2 # Observe floor of odd difference.
+    self.lpad = 1 + self.rpad
+    self.bli = np.concatenate([np.zeros(self.lpad), self.bli, np.zeros(self.rpad)])
     self.blep = np.cumsum(self.bli)
     # Everything is real after we discard the phase info here:
     absdft = np.abs(np.fft.fft(self.bli))
     # The "real cepstrum" is symmetric apart from its first element:
     realcepstrum = np.fft.ifft(np.log(self.minmag + absdft))
-    # Leave first point, zero max phase part, double min phase part to compensate:
-    realcepstrum[1:self.midpoint + 1] *= 2
+    # Leave first point, zero max phase part, double min phase part to compensate.
+    # The midpoint is shared between parts so it doesn't change:
+    realcepstrum[1:self.midpoint] *= 2
     realcepstrum[self.midpoint + 1:] = 0
+    # We subtract the minmag again to preserve height of integral:
     self.minbli = np.fft.ifft(np.exp(np.fft.fft(realcepstrum)) - self.minmag).real
     dtype = np.float32
-    self.minblep = np.append(np.cumsum(self.minbli, dtype = dtype), [1] * (scale - 1))
-    self.minbleps = np.reshape(self.minblep, (scale, order + 1), 'F')
+    self.minblep = np.cumsum(self.minbli, dtype = dtype)
+    ones = (-self.size) % scale
+    self.minblep = np.append(self.minblep, [1] * ones)
+    self.mixinsize = len(self.minblep) // scale
+    self.minbleps = np.reshape(self.minblep, (scale, self.mixinsize), 'F')
     self.idealscale = ctrlrate // fractions.gcd(ctrlrate, outrate)
     self.factor = outrate / ctrlrate * scale
-    self.mixinsize = len(self.minblep[::scale])
     self.scale = scale
 
   def getoutindexandshape(self, ctrlx):
