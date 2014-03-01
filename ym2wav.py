a1bd75410ae174e9c114670ac28482dcf4e47a28
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-03-01 23:23:25 +0000

    console visualisation of chip state

--- ym2wav.py
+++ ym2wav.py
@@ -22,9 +22,63 @@ from pym2149.out import WavWriter
 from pym2149.util import Session
 from pym2149.ymformat import ymopen
 from pym2149.mix import IdealMixer
+from pym2149.pitch import Period
 from cli import Config
+import sys
 
 log = logging.getLogger(__name__)
+
+class Roll:
+
+  shapes = ('\\_',) * 4 + ('/_',) * 4 + ('\\\\', '\\_', '\\/', '\\-', '//', '/-', '/\\', '/_')
+
+  def __init__(self, height, chip, nomclock):
+    self.line = 0
+    self.jump = "\x1b[%sA" % height
+    self.format = ' | '.join(chip.channels * ["%6s %1s %2s %1s %2s %6s"])
+    self.height = height
+    self.chip = chip
+    self.nomclock = nomclock
+
+  def update(self):
+    if self.line == self.height:
+      sys.stderr.write(self.jump)
+      self.line = 0
+    vals = []
+    for c in xrange(self.chip.channels):
+      tone = self.chip.toneflags[c].value
+      noise = self.chip.noiseflags[c].value
+      env = self.chip.levelmodes[c].value
+      level = self.chip.fixedlevels[c].value
+      rhs = env or level
+      if tone and rhs:
+        vals.append(Period(self.chip.toneperiods[c].value).tonefreq(self.nomclock).pitch())
+      else:
+        vals.append('')
+      if tone and noise and rhs:
+        vals.append('&')
+      else:
+        vals.append('')
+      if noise and rhs:
+        vals.append(self.chip.noiseperiod.value)
+      else:
+        vals.append('')
+      if (tone or noise) and rhs:
+        vals.append('*')
+      else:
+        vals.append('')
+      if env:
+        shape = self.chip.envshape.value
+        vals.append(self.shapes[shape])
+        vals.append(Period(self.chip.envperiod.value).envfreq(self.nomclock, shape).pitch())
+      elif level:
+        vals.append(level)
+        vals.append('')
+      else:
+        vals.append('')
+        vals.append('')
+    print >> sys.stderr, self.format % tuple(vals)
+    self.line += 1
 
 def main():
   config = Config()
@@ -37,8 +91,10 @@ def main():
     stream = WavWriter(chip.clock, IdealMixer(chip), outpath)
     try:
       session = Session(chip.clock)
+      roll = Roll(f.framefreq, chip, f.clock)
       for frame in f:
         chip.update(frame)
+        roll.update()
         for b in session.blocks(f.framefreq):
           stream.call(b)
       stream.flush()
