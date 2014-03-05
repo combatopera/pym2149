a14a9a24faa46787b19699c57882d71ac69bf8b3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-03-05 18:17:58 +0000

    refactor

--- samples.py
+++ samples.py
@@ -36,20 +36,27 @@ sawnote = 600 # First peak should have this frequency.
 trinote = 1300 # First peak should have half this frequency.
 slowtrinote = 2 # Frequency and actual period are both 1.
 
-def main():
-  config = Config()
-  chip = config.createchip(nomclock)
-  for c in xrange(chip.channels):
-    chip.toneflags[c].value = False
-    chip.noiseflags[c].value = False
-  chip.toneperiods[0].value = int(round(nomclock / (16 * tonenote)))
-  chip.noiseperiod.value = int(round(nomclock / (16 * noisenote)))
-  chip.fixedlevels[0].value = 15
-  targetpath = os.path.join(os.path.dirname(__file__), 'target')
-  if not os.path.exists(targetpath):
-    os.mkdir(targetpath)
-  def dump(name):
-    path = os.path.join(targetpath, name)
+class Samples:
+
+  def __init__(self):
+    self.config = Config()
+    self.targetpath = os.path.join(os.path.dirname(__file__), 'target')
+    if not os.path.exists(self.targetpath):
+      os.mkdir(self.targetpath)
+
+  def dump(self, sample):
+    chip = self.config.createchip(nomclock)
+    for c in xrange(chip.channels):
+      chip.toneflags[c].value = False
+      chip.noiseflags[c].value = False
+    chip.toneperiods[0].value = int(round(nomclock / (16 * tonenote)))
+    chip.noiseperiod.value = int(round(nomclock / (16 * noisenote)))
+    chip.fixedlevels[0].value = 15
+    sample(chip)
+    name = sample.__name__
+    if name.startswith('_'):
+      name = name[1:]
+    path = os.path.join(self.targetpath, name)
     log.debug(path)
     stream = WavWriter(chip.clock, IdealMixer(chip), path + '.wav')
     try:
@@ -62,29 +69,80 @@ def main():
     finally:
       stream.close()
     subprocess.check_call(['sox', path + '.wav', '-n', 'spectrogram', '-o', path + '.png'])
-  chip.toneflags[0].value = True
-  dump('1ktone')
-  chip.toneflags[0].value = False
-  chip.noiseflags[0].value = True
-  dump('5knoise')
-  chip.toneflags[0].value = True
-  dump('1ktone5knoise')
-  chip.toneflags[0].value = False
-  chip.noiseflags[0].value = False
-  chip.levelmodes[0].value = 1 # Envelope on.
-  chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
-  chip.envshape.value = 0x08
-  dump('600saw')
-  chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
-  chip.envshape.value = 0x10
-  dump('600sin')
-  chip.envperiod.value = int(round(nomclock / (256 * trinote)))
-  chip.envshape.value = 0x0a
-  dump('650tri')
-  chip.envperiod.value = int(round(nomclock / (256 * slowtrinote)))
-  chip.toneflags[0].value = True
-  chip.noiseflags[0].value = True
-  dump('1tri1ktone5knoise')
+
+def main():
+  samples = Samples()
+  def _1ktone(chip):
+    chip.toneflags[0].value = True
+  samples.dump(_1ktone)
+  def _5knoise(chip):
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = True
+  samples.dump(_5knoise)
+  def _1ktone5knoise(chip):
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = True
+    chip.toneflags[0].value = True
+  samples.dump(_1ktone5knoise)
+  def _600saw(chip):
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = True
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = False
+    chip.levelmodes[0].value = 1 # Envelope on.
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x08
+  samples.dump(_600saw)
+  def _600sin(chip):
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = True
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = False
+    chip.levelmodes[0].value = 1 # Envelope on.
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x08
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x10
+  samples.dump(_600sin)
+  def _650tri(chip):
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = True
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = False
+    chip.levelmodes[0].value = 1 # Envelope on.
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x08
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x10
+    chip.envperiod.value = int(round(nomclock / (256 * trinote)))
+    chip.envshape.value = 0x0a
+  samples.dump(_650tri)
+  def _1tri1ktone5knoise(chip):
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = True
+    chip.toneflags[0].value = True
+    chip.toneflags[0].value = False
+    chip.noiseflags[0].value = False
+    chip.levelmodes[0].value = 1 # Envelope on.
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x08
+    chip.envperiod.value = int(round(nomclock / (256 * sawnote)))
+    chip.envshape.value = 0x10
+    chip.envperiod.value = int(round(nomclock / (256 * trinote)))
+    chip.envshape.value = 0x0a
+    chip.envperiod.value = int(round(nomclock / (256 * slowtrinote)))
+    chip.toneflags[0].value = True
+    chip.noiseflags[0].value = True
+  samples.dump(_1tri1ktone5knoise)
 
 if '__main__' == __name__:
   main()
