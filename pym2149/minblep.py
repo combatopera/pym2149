a435efae53c7f2963237c5c85d8749a5372db6ad
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-04-17 08:03:30 +0100

    indent

--- pym2149/minblep.py
+++ pym2149/minblep.py
@@ -95,26 +95,26 @@ def pasteminblepsimpl(n, out, outi, outsize, mixinsize, minblep, shape, amp, sca
   one = 1
   x = zero
   while x < n:
-    a = amp[x]
-    if a != zero:
-      k = (ctrlx + x) % ctrlrate
-      q = (ctrlx + x) // ctrlrate
-      i = q * outrate + outi[k] - out0
-      s = shape[k]
-      j = i + mixinsize
-      if i < j:
-        while 1:
-          out[i] += minblep[s] * a
-          i += one
-          s += scale
-          if i == j:
-            break
-      if i < outsize:
-        while 1:
-          out[i] += a
-          i += one
-          if i == outsize:
-            break
-    x += one
+      a = amp[x]
+      if a != zero:
+        k = (ctrlx + x) % ctrlrate
+        q = (ctrlx + x) // ctrlrate
+        i = q * outrate + outi[k] - out0
+        s = shape[k]
+        j = i + mixinsize
+        if i < j:
+          while 1:
+            out[i] += minblep[s] * a
+            i += one
+            s += scale
+            if i == j:
+              break
+        if i < outsize:
+          while 1:
+            out[i] += a
+            i += one
+            if i == outsize:
+              break
+      x += one
 
 log.debug('Done compiling.')
