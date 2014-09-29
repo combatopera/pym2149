5908309cf01b58a026625e382c3788ed16c49098
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-09-29 23:39:32 +0100

    midi2jack currently sans midi

---
+++ midi2jack.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# Copyright 2014 Andrzej Cichocki
+
+# This file is part of pym2149.
+#
+# pym2149 is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pym2149 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import division
+from pym2149.initlogging import logging
+from pym2149.jackclient import JackClient
+from pym2149.nod import Block
+from config import getprocessconfig
+
+log = logging.getLogger(__name__)
+
+def main():
+  config = getprocessconfig()
+  jackclient = JackClient(config)
+  chip, stream = jackclient.newchipandstream(None)
+  try:
+    minbleps = stream.wavs[0].minbleps
+    naivex = 0
+    while True:
+      # Midi goes here.
+      # Make min amount of chip data to get one JACK block:
+      naiven = minbleps.getminnaiven(naivex, stream.size)
+      stream.call(Block(naiven))
+      naivex = (naivex + naiven) % chip.clock
+  finally:
+    stream.close()
+  jackclient.dispose()
+
+if '__main__' == __name__:
+  main()
