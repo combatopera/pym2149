d3b9b79ce5c24efd08162794b52631b1e20b61f8
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-09-29 23:13:19 +0100

    add test, use arithmetic not looping

--- pym2149/test_minblep.py
+++ pym2149/test_minblep.py
@@ -47,5 +47,28 @@ class TestMinBleps(unittest.TestCase):
     self.assertEqual([0, 0, 1, 1, 2, 3, 3, 4, 4, 5] * 2, [m.outi for m in mixins])
     self.assertEqual([4, 1, 3, 0, 2] * 4, [m.shape for m in mixins])
 
+  def test_counts(self):
+    mb = MinBleps(10, 6, 5)
+    self.assertEqual([0, 0, 1, 1, 2, 3, 3, 4, 4, 5], [mb.getoutcount(0, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 1, 2, 3, 3, 4, 4, 5, 6], [mb.getoutcount(1, n) for n in xrange(10)])
+    self.assertEqual([0, 0, 1, 2, 2, 3, 3, 4, 5, 5], [mb.getoutcount(2, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 2, 2, 3, 3, 4, 5, 5, 6], [mb.getoutcount(3, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 1, 2, 2, 3, 4, 4, 5, 5], [mb.getoutcount(4, n) for n in xrange(10)])
+    self.assertEqual([0, 0, 1, 1, 2, 3, 3, 4, 4, 5], [mb.getoutcount(5, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 1, 2, 3, 3, 4, 4, 5, 6], [mb.getoutcount(6, n) for n in xrange(10)])
+    self.assertEqual([0, 0, 1, 2, 2, 3, 3, 4, 5, 5], [mb.getoutcount(7, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 2, 2, 3, 3, 4, 5, 5, 6], [mb.getoutcount(8, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 1, 2, 2, 3, 4, 4, 5, 5], [mb.getoutcount(9, n) for n in xrange(10)])
+    self.assertEqual([0, 2, 4, 5, 7, 9, 10, 12, 14, 15], [mb.getminnaiven(0, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 3, 4, 6, 8, 9, 11, 13, 14], [mb.getminnaiven(1, n) for n in xrange(10)])
+    self.assertEqual([0, 2, 3, 5, 7, 8, 10, 12, 13, 15], [mb.getminnaiven(2, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 2, 4, 6, 7, 9, 11, 12, 14], [mb.getminnaiven(3, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 3, 5, 6, 8, 10, 11, 13, 15], [mb.getminnaiven(4, n) for n in xrange(10)])
+    self.assertEqual([0, 2, 4, 5, 7, 9, 10, 12, 14, 15], [mb.getminnaiven(5, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 3, 4, 6, 8, 9, 11, 13, 14], [mb.getminnaiven(6, n) for n in xrange(10)])
+    self.assertEqual([0, 2, 3, 5, 7, 8, 10, 12, 13, 15], [mb.getminnaiven(7, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 2, 4, 6, 7, 9, 11, 12, 14], [mb.getminnaiven(8, n) for n in xrange(10)])
+    self.assertEqual([0, 1, 3, 5, 6, 8, 10, 11, 13, 15], [mb.getminnaiven(9, n) for n in xrange(10)])
+
 if __name__ == '__main__':
   unittest.main()
