473ac79d4d2edab7d7794319791d57b52001aedf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-12-16 20:27:25 +0000

    print working dirs

--- drone.io.sh
+++ drone.io.sh
@@ -2,13 +2,15 @@
 
 set -e
 
-cd ..
+pwd
+
+cd ..; pwd
 
 hg clone https://bitbucket.org/combatopera/devutils
 
-cd -
+cd -; pwd
 
-cd
+cd; pwd
 
 wget http://repo.continuum.io/miniconda/Miniconda-3.0.5-Linux-x86_64.sh
 
@@ -16,7 +18,7 @@ bash Miniconda-3.0.5-Linux-x86_64.sh <<<$'\nyes\nminiconda\nno\n'
 
 PATH="$PWD/miniconda/bin:$PATH"
 
-cd -
+cd -; pwd
 
 conda install numpy cython pyflakes nose
 
