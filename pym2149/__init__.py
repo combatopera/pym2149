23aeb1506123a4e0071d98d7b42b280fc34eacb9
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2014-12-25 15:50:35 +0000

    save binaries in-place, where we can check their mtime

--- pym2149/__init__.py
+++ pym2149/__init__.py
@@ -1,4 +1,4 @@
 import pyximport, numpy as np
 
 # Note -O3 is apparently the default:
-pyximport.install(setup_args = {'include_dirs': np.get_include()})
+pyximport.install(setup_args = {'include_dirs': np.get_include()}, inplace = True)
