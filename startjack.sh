0bd3d4f710e6211485a0c195aa83ffd16ed8f424
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-01-14 18:43:25 +0000

    script to idempotently start jackd (and qjackctl)

---
+++ startjack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+seconds=5
+blurb="$seconds second delay: "
+
+pidof jackd || {
+
+    qjackctl -s &>/dev/null &
+
+    echo -n "$blurb"
+    for s in $(seq 1 $seconds); do
+        echo -n $'\u2591'
+    done
+    echo -n $'\r'"$blurb"
+
+    for s in $(seq 1 $seconds); do
+
+        sleep 1
+
+        echo -n $'\u2588'
+
+    done
+
+    echo
+
+}
