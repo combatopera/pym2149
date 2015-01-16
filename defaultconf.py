f4c0a4b8749e8b38b07bfaa61a8ec2eab5397080
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-01-16 11:54:59 +0000

    comments

--- defaultconf.py
+++ defaultconf.py
@@ -22,8 +22,8 @@ from pym2149 import midichannelcount
 outputrate = 44100
 '''May be ignored e.g. when sending data to JACK we must use its own rate.'''
 
-freqclamp = True # XXX: Does this sound worse, particularly at lower outputrates?
-'''Whether inaudible tones are clamped to the lowest such tone. Setting this to True improves performance when very high frequency tones are used to attenuate the envelope.'''
+freqclamp = True
+'''Whether inaudible tones are clamped to the lowest such tone. Setting this to True improves performance when very high frequency tones are used to attenuate the envelope. Quality may be degraded, particularly at lower outputrates.'''
 
 underclock = 8
 '''The number of nominal clock ticks until the chip state can next be updated, must be a factor of 8 i.e. in {1, 2, 4, 8}. Higher numbers improve performance but the authentic setting is currently unknown.'''
@@ -32,7 +32,7 @@ oscpause = False
 '''Whether an oscillator is paused when turned off in the mixer. This option doesn't significantly help performance so it's a bit useless.'''
 
 defaultclock = stclock
-'''Used when there is no context clock or override.'''
+'''Used when there is no context clock (e.g. specified by a file being played) or override.'''
 
 clockoverrideornone = None
 '''If not None, override the context clock.'''
