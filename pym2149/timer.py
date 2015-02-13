b6bd5131ba3a078324b1f7cc293eb7facba7b3d4
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-02-13 09:13:29 +0000

    split into simple and minblockrate impls

--- pym2149/timer.py
+++ pym2149/timer.py
@@ -21,33 +21,39 @@ import logging, inspect
 
 log = logging.getLogger(__name__)
 
-class Timer:
+class Timer: pass
 
-  def __init__(self, clock, minblockrate = 100):
-    if minblockrate is not None:
-      # If division not exact, rate will slightly exceed given minimum:
-      self.maxblocksize = int(clock // minblockrate)
-      if not self.maxblocksize:
-        raise Exception(clock, minblockrate)
-    else:
-      self.maxblocksize = None
-    self.carryticks = 0
-    self.clock = clock
+class SimpleTimer(Timer):
 
-  def blocksforperiod(self, refreshrate):
-    available = self.carryticks + self.clock
-    blockticks = int(round(available / refreshrate))
-    self.carryticks = available - blockticks * refreshrate
-    if self.maxblocksize is not None:
-      while blockticks:
-        size = min(blockticks, self.maxblocksize)
-        b = Block(size)
-        blockticks -= size
-        yield b
-    else:
-      yield Block(blockticks)
+    def __init__(self, clock):
+        self.carryticks = 0
+        self.clock = clock
 
-  def __del__(self): # XXX: Reliable enough?
-    if self.carryticks:
-      context = inspect.stack()[1]
-      log.warn("Non-zero end of session carry %s in %s before line %s.", self.carryticks, context[1], context[2])
+    def blocksforperiod(self, refreshrate):
+        available = self.carryticks + self.clock
+        blockticks = int(round(available / refreshrate))
+        self.carryticks = available - blockticks * refreshrate
+        yield Block(blockticks)
+
+    def __del__(self): # XXX: Reliable enough?
+        if self.carryticks:
+            context = inspect.stack()[1]
+            log.warn("Non-zero end of session carry %s in %s before line %s.", self.carryticks, context[1], context[2])
+
+class MinBlockRateTimer(SimpleTimer):
+
+    def __init__(self, clock, minblockrate):
+        # If division not exact, rate will slightly exceed given minimum:
+        self.maxblocksize = int(clock // minblockrate)
+        if not self.maxblocksize:
+            raise Exception(clock, minblockrate)
+        SimpleTimer.__init__(self, clock)
+
+    def blocksforperiod(self, refreshrate):
+        wholeperiodblock, = SimpleTimer.blocksforperiod(self, refreshrate)
+        blockticks = wholeperiodblock.framecount
+        while blockticks:
+            size = min(blockticks, self.maxblocksize)
+            b = Block(size)
+            blockticks -= size
+            yield b
