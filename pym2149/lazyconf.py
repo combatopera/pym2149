a52825efd320d1a0a7cc7dd183fd80b7c70ef524
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-02-23 18:59:52 +0000

    not used

--- pym2149/lazyconf.py
+++ pym2149/lazyconf.py
@@ -41,8 +41,6 @@ class View:
         self.loader = loader
 
     def __getattr__(self, name):
-        if 'configpath' == name:
-            return self.loader.paths[-1]
         context = self
         obj = self.loader.expressions[name](context)
         for mod in self.loader.modifiers(name):
