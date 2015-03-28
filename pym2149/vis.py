32633e1bd2aaf72ae4e423670b75efb03d3f134e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-03-28 12:29:33 +0000

    don't rely on old-style division

--- pym2149/vis.py
+++ pym2149/vis.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import division
 from pitch import Period, Freq
 from di import types
 from iface import Chip, Config
