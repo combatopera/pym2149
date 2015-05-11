035e9898bc87fbc8ba2cf56c0393f61025a480af
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-05-11 19:01:04 +0100

    reindent

--- pym2149/pitch.py
+++ pym2149/pitch.py
@@ -22,85 +22,85 @@ import math
 
 class Pitch(float):
 
-  a4freq = 440
-  a4midi = 69
-  names = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
+    a4freq = 440
+    a4midi = 69
+    names = ('C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B')
 
-  def freq(self):
-    return Freq(self.a4freq * (2 ** ((self - self.a4midi) / 12)))
+    def freq(self):
+        return Freq(self.a4freq * (2 ** ((self - self.a4midi) / 12)))
 
-  def __add__(self, that):
-    return self.__class__(float.__add__(self, that))
+    def __add__(self, that):
+        return self.__class__(float.__add__(self, that))
 
-  def __sub__(self, that):
-    return self.__class__(float.__sub__(self, that))
+    def __sub__(self, that):
+        return self.__class__(float.__sub__(self, that))
 
-  def __str__(self):
-    nearest = int(math.ceil(self - .5))
-    octave = nearest // 12
-    note = nearest % 12
-    cents = int(round((self - nearest) * 100))
-    octave -= 1
-    notestr = self.names[note]
-    if len(notestr) < 2:
-      notestr += '.'
-    octavestr = str(octave)
-    if len(octavestr) < 2:
-      octavestr = '.' + octavestr
-    if abs(cents) < 10:
-      centsstr = ' ' * 3
-    else:
-      centsstr = "%+d" % cents
-    return notestr + octavestr + centsstr
+    def __str__(self):
+        nearest = int(math.ceil(self - .5))
+        octave = nearest // 12
+        note = nearest % 12
+        cents = int(round((self - nearest) * 100))
+        octave -= 1
+        notestr = self.names[note]
+        if len(notestr) < 2:
+            notestr += '.'
+        octavestr = str(octave)
+        if len(octavestr) < 2:
+            octavestr = '.' + octavestr
+        if abs(cents) < 10:
+            centsstr = ' ' * 3
+        else:
+            centsstr = "%+d" % cents
+        return notestr + octavestr + centsstr
 
 @singleton
 class shapescale:
 
-  trishapes = frozenset([0xa, 0xe])
+    trishapes = frozenset([0xa, 0xe])
 
-  def __call__(self, shape):
-    # Musically, the triangular shapes have twice the scale:
-    return (256, 512)[shape in self.trishapes]
+    def __call__(self, shape):
+        # Musically, the triangular shapes have twice the scale:
+        return (256, 512)[shape in self.trishapes]
 
 class Freq(float):
 
-  def periodimpl(self, clock, scale):
-    return Period(round(clock / (scale * self)))
+    def periodimpl(self, clock, scale):
+        return Period(round(clock / (scale * self)))
 
-  def toneperiod(self, clock):
-    return self.periodimpl(clock, 16)
+    def toneperiod(self, clock):
+        return self.periodimpl(clock, 16)
 
-  def noiseperiod(self, clock):
-    return self.periodimpl(clock, 16) # First notch at freq.
+    def noiseperiod(self, clock):
+        return self.periodimpl(clock, 16) # First notch at freq.
 
-  def envperiod(self, clock, shape):
-    return self.periodimpl(clock, shapescale(shape))
+    def envperiod(self, clock, shape):
+        return self.periodimpl(clock, shapescale(shape))
 
-  def rtoneperiod(self):
-    steps = 2 # TODO LATER: Other shapes will have different numbers of steps.
-    diff = None
-    for prescaler in prescalers:
-      if prescaler:
-        prescaler *= steps # Avoid having to multiply twice.
-        tdr = int(round(mfpclock / (self * prescaler)))
-        if 1 <= tdr and tdr <= 255:
-          rtp = tdr * prescaler
-          d = abs(mfpclock / rtp - self)
-          if diff is None or d < diff:
-            rtoneperiod = rtp
-            diff = d
-    return rtoneperiod
+    def rtoneperiod(self):
+        steps = 2 # TODO LATER: Other shapes will have different numbers of steps.
+        diff = None
+        for prescaler in prescalers:
+            if prescaler:
+                prescaler *= steps # Avoid having to multiply twice.
+                tdr = int(round(mfpclock / (self * prescaler)))
+                if 1 <= tdr and tdr <= 255:
+                    rtp = tdr * prescaler
+                    d = abs(mfpclock / rtp - self)
+                    if diff is None or d < diff:
+                        rtoneperiod = rtp
+                        diff = d
+        return rtoneperiod
 
-  def pitch(self):
-    return Pitch(Pitch.a4midi + 12 * math.log(self / Pitch.a4freq, 2))
+    def pitch(self):
+        return Pitch(Pitch.a4midi + 12 * math.log(self / Pitch.a4freq, 2))
 
-  def __mul__(self, that):
-    return self.__class__(float.__mul__(self, that))
+    def __mul__(self, that):
+        return self.__class__(float.__mul__(self, that))
 
 class Period(int):
 
-  def tonefreq(self, clock):
-    return Freq(clock / (16 * self))
+    def tonefreq(self, clock):
+        return Freq(clock / (16 * self))
 
-  def envfreq(self, clock, shape):
-    return Freq(clock / (shapescale(shape) * self))
+    def envfreq(self, clock, shape):
+        return Freq(clock / (shapescale(shape) * self))
