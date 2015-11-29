f1cff54edf00162367dea738d22d8e3d1e741665
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-11-29 16:45:22 +0000

    refactoring

--- pym2149/speed.py
+++ pym2149/speed.py
@@ -35,17 +35,16 @@ class Shape:
         def g():
             for i in xrange(periods * speed + 1):
                 yield -1 if i % speed else 1
-        self.shape = np.fromiter(g(), dtype = dtype)
+        self.kernel = np.fromiter(g(), dtype = dtype)
         self.speed = speed
 
     def getscore(self, v):
-        return max((score, self.speed - 1 - phase) for phase, score in enumerate(np.correlate(self.shape, v[:self.speed * (periods + 1)])))
+        return max((score, self.speed - 1 - phase) for phase, score in enumerate(np.correlate(self.kernel, v[:self.speed * (periods + 1)])))
 
 class SpeedDetector:
 
     def __init__(self, maxspeed, callback = defaultcallback):
         self.shapes = [Shape(speed) for speed in xrange(1, maxspeed + 1)]
-        #for shape in self.shapes: print ''.join(chr(ord('0') + x) if x else '.' for x in shape.shape)
         self.history = np.zeros(maxspeed * (periods + 1), dtype = dtype)
         self.speed = None
         self.index = 0
@@ -54,20 +53,12 @@ class SpeedDetector:
     def __call__(self, eventcount):
         self.history[1:] = self.history[:-1] # Lose the oldest value.
         self.history[0] = eventcount # New newest value.
-        #print ''.join(chr(ord('0') + x) if x else '.' for x in self.history)
         scores = sorted((shape.getscore(self.history), shape.speed) for shape in self.shapes)
         scores = [(score, (speed, (self.index - phase) % speed)) for (score, phase), speed in scores]
         newspeed = scores[-1][1]
-        #print self.history,newspeed
-        #print ' '.join("%.3f=%s"%s for s in scores),
         accept = scores[-1][0] and scores[-1][0] >= scores[-2][0] * clarity
-        #if accept:
-        #    print 'OK'
-        #else:
-        #    print
         self.index += 1
         if newspeed != self.speed and accept:
-            print self.history, scores[-3:]
             oldspeed = self.speed
             self.speed = newspeed
             self.callback(oldspeed, self.speed)
