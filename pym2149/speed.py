7ff2d53fca2fc8e1e6e292e5c1069505990ed2f2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2015-11-29 17:03:58 +0000

    refactoring

--- pym2149/speed.py
+++ pym2149/speed.py
@@ -16,6 +16,7 @@
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging, numpy as np
+from collections import namedtuple
 
 log = logging.getLogger(__name__)
 
@@ -25,27 +26,31 @@ def defaultcallback(oldspeedornone, speed):
     else:
         log.warn("Speed was %s but is now: %s", oldspeedornone, speed)
 
-periods = 2
-dtype = np.int32
+kernelperiods = 2
+dtype = np.int32 # Must be signed.
 clarity = 1.1
+
+ScoreSpeedPhase = namedtuple('ScoreSpeedPhase', 'score speed phase')
 
 class Shape:
 
     def __init__(self, speed):
         def g():
-            for i in xrange(periods * speed + 1):
+            for i in xrange(kernelperiods * speed + 1):
                 yield -1 if i % speed else 1
         self.kernel = np.fromiter(g(), dtype = dtype)
+        self.window = speed * (kernelperiods + 1)
         self.speed = speed
 
-    def getscore(self, v):
-        return max((score, self.speed - 1 - phase) for phase, score in enumerate(np.correlate(self.kernel, v[:self.speed * (periods + 1)])))
+    def bestscorephase(self, history, index):
+        score, phase = max((score, phase) for phase, score in enumerate(np.correlate(self.kernel, history[:self.window])))
+        return ScoreSpeedPhase(score, self.speed, (index + phase + 1) % self.speed)
 
 class SpeedDetector:
 
     def __init__(self, maxspeed, callback = defaultcallback):
         self.shapes = [Shape(speed) for speed in xrange(1, maxspeed + 1)]
-        self.history = np.zeros(maxspeed * (periods + 1), dtype = dtype)
+        self.history = np.zeros(maxspeed * (kernelperiods + 1), dtype = dtype)
         self.speed = None
         self.index = 0
         self.callback = callback
@@ -53,11 +58,10 @@ class SpeedDetector:
     def __call__(self, eventcount):
         self.history[1:] = self.history[:-1] # Lose the oldest value.
         self.history[0] = eventcount # New newest value.
-        scores = sorted((shape.getscore(self.history), shape.speed) for shape in self.shapes)
-        scores = [(score, (speed, (self.index - phase) % speed)) for (score, phase), speed in scores]
-        newspeed = scores[-1][1]
-        accept = scores[-1][0] and scores[-1][0] >= scores[-2][0] * clarity
+        scorespeedphasetuples = sorted(shape.bestscorephase(self.history, self.index) for shape in self.shapes)
         self.index += 1
+        newspeed = scorespeedphasetuples[-1].speed, scorespeedphasetuples[-1].phase
+        accept = scorespeedphasetuples[-1].score and scorespeedphasetuples[-1].score >= scorespeedphasetuples[-2].score * clarity
         if newspeed != self.speed and accept:
             oldspeed = self.speed
             self.speed = newspeed
