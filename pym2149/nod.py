a5b3d2b782f5dd41c195a55e0fcc2a033974dc2a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-01-12 08:14:34 +0000

    unlikely to ever happen

--- pym2149/nod.py
+++ pym2149/nod.py
@@ -29,7 +29,6 @@ class Block:
   def __repr__(self):
     return "%s(%r)" % (self.__class__.__name__, self.framecount)
 
-# TODO: Automagic node graph compilation to limit height of call stack.
 class Node:
 
   def __init__(self):
