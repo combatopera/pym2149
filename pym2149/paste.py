607cd6927d4f9922d15ae394877dee824f8a1550
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2016-01-26 21:42:40 +0000

    use u4

--- pym2149/paste.py
+++ pym2149/paste.py
@@ -17,10 +17,11 @@
 
 import numpy as np
 from pyrbo import turbo, X
+from const import u4
 
 mixinsize = None
 
-@turbo(ampsize = np.uint32, outp = [np.float32], naivex2outxp = [np.int32], outsize = np.uint32, demultiplexedp = [np.float32], naivex2offp = [np.int32], ampp = [np.float32], naivex = np.uint32, naiverate = np.uint32, outrate = np.uint32, out0 = np.uint32, dclevel = np.float32, dcindex = np.uint32, ampchunk = np.uint32, a = np.float32, i = np.uint32, dccount = np.uint32, mixinp = [np.float32], mixinsize = X)
+@turbo(ampsize = u4, outp = [np.float32], naivex2outxp = [np.int32], outsize = u4, demultiplexedp = [np.float32], naivex2offp = [np.int32], ampp = [np.float32], naivex = u4, naiverate = u4, outrate = u4, out0 = u4, dclevel = np.float32, dcindex = u4, ampchunk = u4, a = np.float32, i = u4, dccount = u4, mixinp = [np.float32], mixinsize = X)
 def pasteminbleps(ampsize, outp, naivex2outxp, outsize, demultiplexedp, naivex2offp, ampp, naivex, naiverate, outrate):
   # TODO: This code needs tests.
   out0 = naivex2outxp[naivex]
