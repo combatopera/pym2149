15486c9d34b1c09d963f8f481b7982072bcd757a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-01-17 11:28:24 +0000

    removing midi-specific stuff

--- pym2149/tidal.py
+++ pym2149/tidal.py
@@ -26,118 +26,12 @@ from minblep import MinBleps
 from timer import Timer
 from util import EMA
 from speed import SpeedDetector
+from midi import MidiSchedule
 import native.calsa as calsa, logging, time
 
 log = logging.getLogger(__name__)
 
-class MidiSchedule:
-
-    maxdelay = .01
-    targetlatency = .01 # Conservative?
-
-    def __init__(self, updaterate, skipenabled):
-        self.period = 1 / updaterate
-        self.taketimeema = EMA(.1, time.time())
-        self.skipenabled = skipenabled
-
-    def awaittaketime(self):
-        skipped = 0
-        while True:
-            now = time.time()
-            if now < self.taketimeema.value:
-                time.sleep(self.taketimeema.value - now)
-            elif self.skipenabled and now > self.taketimeema.value + self.maxdelay:
-                # Forget sync with current update, try the next one:
-                self.taketimeema.value += self.period
-                skipped += 1
-            else: # We're in the [0, maxdelay] window (or late if skip disabled).
-                break
-        if skipped:
-            log.warn("Skipped sync with %s updates.", skipped)
-        return now
-
-    def step(self, idealtaketime):
-        # TODO: Instead of EMA, improve sync with PLL.
-        self.taketimeema.value += self.period
-        self.taketimeema(idealtaketime + self.targetlatency)
-
-class ChannelMessage:
-
-    def __init__(self, midi, event):
-        self.midichan = midi.chanbase + event.channel
-
-class ChannelStateMessage(ChannelMessage): pass
-
-class NoteOnOff(ChannelMessage):
-
-    def __init__(self, midi, event):
-        ChannelMessage.__init__(self, midi, event)
-        self.midinote = event.note
-        self.vel = event.velocity
-
-    def __str__(self):
-        return "%s %2d %3d %3d" % (self.char, self.midichan, self.midinote, self.vel)
-
-class NoteOn(NoteOnOff):
-
-    char = 'I'
-
-    def __call__(self, channels):
-        return channels.noteon(self.midichan, self.midinote, self.vel)
-
-class NoteOff(NoteOnOff):
-
-    char = 'O'
-
-    def __call__(self, channels):
-        return channels.noteoff(self.midichan, self.midinote, self.vel)
-
-class PitchBend(ChannelStateMessage):
-
-    def __init__(self, midi, event):
-        ChannelStateMessage.__init__(self, midi, event)
-        self.bend = event.value # In [-0x2000, 0x2000).
-
-    def __call__(self, channels):
-        return channels.pitchbend(self.midichan, self.bend)
-
-    def __str__(self):
-        return "B %2d %5d" % (self.midichan, self.bend)
-
-class ProgramChange(ChannelStateMessage):
-
-    def __init__(self, midi, event):
-        ChannelStateMessage.__init__(self, midi, event)
-        self.program = midi.programbase + event.value
-
-    def __call__(self, channels):
-        return channels.programchange(self.midichan, self.program)
-
-    def __str__(self):
-        return "P %2d %3d" % (self.midichan, self.program)
-
-class ControlChange(ChannelStateMessage):
-
-    def __init__(self, midi, event):
-        ChannelStateMessage.__init__(self, midi, event)
-        self.controller = event.param
-        self.value = event.value
-
-    def __call__(self, channels):
-        return channels.controlchange(self.midichan, self.controller, self.value)
-
-    def __str__(self):
-        return "C %2d %3d %3d" % (self.midichan, self.controller, self.value)
-
-class MidiListen(SimpleBackground):
-
-    classes = {
-        calsa.SND_SEQ_EVENT_NOTEON: NoteOn,
-        calsa.SND_SEQ_EVENT_NOTEOFF: NoteOff,
-        calsa.SND_SEQ_EVENT_PITCHBEND: PitchBend,
-        calsa.SND_SEQ_EVENT_PGMCHANGE: ProgramChange,
-        calsa.SND_SEQ_EVENT_CONTROLLER: ControlChange,
-    }
+class TidalListen(SimpleBackground):
 
     @types(Config, PLL)
     def __init__(self, config, pll):
@@ -157,9 +51,9 @@ class MidiListen(SimpleBackground):
                 eventobj = self.classes[event.type](self, event)
                 self.pll.event(event.time, eventobj, eventobj.midichan not in self.pllignoremidichans)
 
-class MidiPump(MainBackground):
+class TidalPump(MainBackground):
 
-    @types(Config, MidiListen, Channels, MinBleps, Stream, Chip, Timer, PLL)
+    @types(Config, TidalListen, Channels, MinBleps, Stream, Chip, Timer, PLL)
     def __init__(self, config, midi, channels, minbleps, stream, chip, timer, pll):
         MainBackground.__init__(self, config)
         self.updaterate = config.updaterate
