a0fae062f328465083a2dc7ef8be018cf57a01ff
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-01-17 20:20:35 +0000

    parse osc messages

--- pym2149/tidal.py
+++ pym2149/tidal.py
@@ -15,12 +15,13 @@
 # You should have received a copy of the GNU General Public License
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
+from __future__ import division
 from diapyr import types
 from pll import PLL
 from bg import SimpleBackground
 from iface import Config
 from midi import NoteOn
-import logging, time, subprocess, os, tempfile, socket, udppump, sys
+import logging, time, subprocess, os, tempfile, socket, udppump, sys, struct
 
 log = logging.getLogger(__name__)
 
@@ -53,13 +54,88 @@ class TidalClient:
     def read(self):
         while self.open:
             try:
-                print self.sock.recvfrom(udppump.bufsize)[0]
+                v = self.sock.recvfrom(udppump.bufsize)[0]
             except socket.timeout:
-                pass
+                continue
+            if not v.startswith('/status\0'):
+                print parse(v)
 
     def interrupt(self):
         subprocess.check_call(['sudo', 'kill', str(self.sniffer.pid)])
         self.open = False
+
+def parse(v):
+    return (Bundle if v.startswith('#bundle\0') else Message)(v)
+
+class Reader:
+
+    seconds1970 = 25567 * 24 * 60 * 60
+    fractionlimit = 1 << 32
+
+    def __init__(self, v):
+        self.c = 0
+        self.v = v
+
+    def consume(self, n):
+        self.c += n
+        return self.v[self.c - n:self.c]
+
+    def timetag(self):
+        seconds1900, fraction = struct.unpack('>II', self.consume(8))
+        return seconds1900 - self.seconds1970 + fraction / self.fractionlimit
+
+    def int32(self):
+        return struct.unpack('>i', self.consume(4))[0]
+
+    def __nonzero__(self):
+        return self.c < len(self.v)
+
+    def element(self):
+        return parse(self.consume(self.int32()))
+
+    def string(self):
+        text = self.consume(self.v.index('\0', self.c) - self.c).decode('ascii')
+        self.c += 1 # Consume at least one null.
+        self.align()
+        return text
+
+    def align(self):
+        while self.c % 4:
+            self.c += 1
+
+    def float32(self):
+        return struct.unpack('>f', self.consume(4))[0]
+
+    def blob(self):
+        blob = self.consume(self.int32())
+        self.align()
+        return blob
+
+class Bundle:
+
+    def __init__(self, v):
+        r = Reader(v)
+        r.string()
+        self.timetag = r.timetag()
+        self.elements = []
+        while r:
+            self.elements.append(r.element())
+
+class Message:
+
+    types = {
+        'i': Reader.int32,
+        's': Reader.string,
+        'f': Reader.float32,
+        'b': Reader.blob,
+    }
+
+    def __init__(self, v):
+        r = Reader(v)
+        self.addrpattern = r.string()
+        self.args = []
+        for tt in r.string()[1:]:
+            self.args.append(self.types[tt](r))
 
 class TidalListen(SimpleBackground):
 
