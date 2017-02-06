754424cbe458a13161d4658998e323e00081d711
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-02-06 22:03:32 +0000

    support arbitrary shape

--- pym2149/osc2.py
+++ pym2149/osc2.py
@@ -19,46 +19,58 @@ from nod import BufNode
 from ring import signaldtype
 from pyrbo import turbo, LOCAL
 from const import u4
+import numpy as np
+
+class Shape:
+
+    def __init__(self, buf):
+        self.size = buf.size
+        self.buf = buf
 
 class ToneOsc(BufNode):
 
+    shape = Shape(np.array([1, 0], signaldtype))
+
     def __init__(self, scale, periodreg):
         BufNode.__init__(self, signaldtype)
-        self.value = 1
+        self.index = 0
         self.progress = 0
         self.scale = scale
         self.periodreg = periodreg
 
     def callimpl(self):
-        self.value, self.progress = self.toneimpl()
+        self.index, self.progress = self.toneimpl()
 
-    @turbo(self = dict(blockbuf = dict(buf = [signaldtype]), block = dict(framecount = u4), value = signaldtype, progress = u4, scale = u4, periodreg = dict(value = u4)), stepsize = u4, i = u4, j = u4, n = u4)
+    @turbo(self = dict(blockbuf = dict(buf = [signaldtype]), block = dict(framecount = u4), index = u4, progress = u4, scale = u4, periodreg = dict(value = u4), shape = dict(size = u4, buf = [signaldtype])), stepsize = u4, i = u4, j = u4, n = u4, val = signaldtype)
     def toneimpl(self):
-        self_blockbuf_buf = self_block_framecount = self_value = self_progress = self_scale = self_periodreg_value = LOCAL
+        self_blockbuf_buf = self_block_framecount = self_index = self_progress = self_scale = self_periodreg_value = self_shape_size = self_shape_buf = LOCAL
         stepsize = self_periodreg_value * self_scale
         i = 0
         if self_progress < stepsize:
+            val = self_shape_buf[self_index]
             j = min(stepsize - self_progress, self_block_framecount)
             while i < j:
-                self_blockbuf_buf[i] = self_value
+                self_blockbuf_buf[i] = val
                 i += 1
         if i == self_block_framecount:
             self_progress += self_block_framecount
         else:
-            self_value = 1 - self_value
+            self_index = (self_index + 1) % self_shape_size
+            val = self_shape_buf[self_index]
             n = (self_block_framecount - i) // stepsize
             while n:
                 j = i + stepsize
                 while i < j:
-                    self_blockbuf_buf[i] = self_value
+                    self_blockbuf_buf[i] = val
                     i += 1
-                self_value = 1 - self_value
+                self_index = (self_index + 1) % self_shape_size
+                val = self_shape_buf[self_index]
                 n -= 1
             self_progress = self_block_framecount - i
             while i < self_block_framecount:
-                self_blockbuf_buf[i] = self_value
+                self_blockbuf_buf[i] = val
                 i += 1
-        return self_value, self_progress
+        return self_index, self_progress
 
 class NoiseOsc(BufNode):
 
