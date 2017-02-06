6af6dea1d2657b593ce7da53fad747997db64c35
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-02-06 16:26:54 +0000

    refactor

--- pym2149/osc2.py
+++ pym2149/osc2.py
@@ -25,20 +25,21 @@ class ToneOsc(BufNode):
 
     def __init__(self, scale, periodreg):
         BufNode.__init__(self, signaldtype)
-        self.value = 0
-        self.countdown = 0
+        self.value = 1
+        self.progress = 0
         self.scale = scale
         self.periodreg = periodreg
 
     def callimpl(self):
-        self.value, self.countdown = self.callturbo()
+        self.value, self.progress = self.callturbo()
 
-    @turbo(self = dict(blockbuf = dict(buf = [signaldtype]), block = dict(framecount = u4), value = signaldtype, countdown = u4, scale = u4, periodreg = dict(value = u4)), i = u4)
+    @turbo(self = dict(blockbuf = dict(buf = [signaldtype]), block = dict(framecount = u4), value = signaldtype, progress = u4, scale = u4, periodreg = dict(value = u4)), stepsize = u4, i = u4)
     def callturbo(self):
+        stepsize = self_periodreg_value * self_scale
         for i in xrange(self_block_framecount):
-            if not self_countdown:
-                self_countdown = self_periodreg_value * self_scale
+            if self_progress == stepsize:
                 self_value = 1 - self_value
+                self_progress = 0
             self_blockbuf_buf[i] = self_value
-            self_countdown -= 1
-        return self_value, self_countdown
+            self_progress += 1
+        return self_value, self_progress
