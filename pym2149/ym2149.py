45c251ffeae8408e9556e554e9776350b9293797
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-02-07 11:27:33 +0000

    actually use new NoiseOsc

--- pym2149/ym2149.py
+++ pym2149/ym2149.py
@@ -16,8 +16,8 @@
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
 from reg import Reg, VersionReg
-from osc import NoiseOsc, EnvOsc, RToneOsc
-from osc2 import ToneOsc, Shape
+from osc import EnvOsc, RToneOsc
+from osc2 import ToneOsc, NoiseOsc, Shape
 from dac import Level, Dac
 from mix import BinMix
 from nod import Container
