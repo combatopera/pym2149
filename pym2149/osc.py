b641b57510157a95d6408e5ace2bc7ffc2788129
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2017-02-10 13:58:15 +0000

    obsolete

--- pym2149/osc.py
+++ pym2149/osc.py
@@ -15,12 +15,11 @@
 # You should have received a copy of the GNU General Public License
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
-import itertools, numpy as np
+import numpy as np
 from nod import BufNode
-from ring import DerivativeRing, derivativedtype, signaldtype
+from ring import derivativedtype, signaldtype
 from buf import MasterBuf
 from mfp import mfpclock
-from shapes import toneshape
 from util import ceildiv
 from pyrbo import turbo
 from const import u4
@@ -43,39 +42,6 @@ class DerivativeNode(BufNode):
 
     def integral(self, signalbuf):
         signalbuf.integrate(self.blockbuf)
-
-class SimpleDerivative(DerivativeNode):
-
-    def __init__(self, scaleofstep, periodreg, eagerstepsize):
-        DerivativeNode.__init__(self)
-        self.scaleofstep = scaleofstep
-        self.periodreg = periodreg
-        self.eagerstepsize = eagerstepsize
-
-    def resetprogress(self):
-        self.progress = 0
-
-    def updatestepsize(self, eager):
-        if eager == self.eagerstepsize:
-            self.stepsize = self.scaleofstep * self.periodreg.value
-
-    def callimpl(self):
-        self.updatestepsize(True)
-        if not self.progress:
-            stepindex = 0
-        else:
-            # If progress beats the new stepsize, we step right away:
-            stepindex = max(0, self.stepsize - self.progress)
-        if stepindex >= self.block.framecount:
-            # Next step of waveform is beyond this block:
-            self.progress += self.block.framecount
-            return self.hold
-        else:
-            self.updatestepsize(False)
-            stepcount = (self.block.framecount - stepindex + self.stepsize - 1) // self.stepsize
-            self.ringcursor.putstrided(self.blockbuf, stepindex, self.stepsize, stepcount)
-            self.progress = (self.block.framecount - stepindex) % self.stepsize
-            return self.integral
 
 class RationalDerivative(DerivativeNode):
 
@@ -160,44 +126,3 @@ class RToneOsc(IntegralNode):
 
     def __init__(self, chipimplclock, timer):
         IntegralNode.__init__(self, RationalDerivative(chipimplclock, timer))
-
-class ToneOsc(IntegralNode):
-
-    def __init__(self, scale, periodreg):
-        scaleofstep = scale * 2 // 2 # Normally half of 16.
-        IntegralNode.__init__(self, SimpleDerivative(scaleofstep, periodreg, True).reset(toneshape))
-
-class NoiseOsc(IntegralNode):
-
-    def __init__(self, scale, periodreg, noiseshape):
-        scaleofstep = scale * 2 # This results in authentic spectrum, see qnoispec.
-        IntegralNode.__init__(self, SimpleDerivative(scaleofstep, periodreg, False).reset(noiseshape))
-
-class EnvOsc(IntegralNode):
-
-    steps = 32
-    shapes = {
-        0x0c: DerivativeRing(xrange(steps)),
-        0x08: DerivativeRing(xrange(steps - 1, -1, -1)),
-        0x0e: DerivativeRing(itertools.chain(xrange(steps), xrange(steps - 1, -1, -1))),
-        0x0a: DerivativeRing(itertools.chain(xrange(steps - 1, -1, -1), xrange(steps))),
-        0x0f: DerivativeRing(itertools.chain(xrange(steps), [0]), steps),
-        0x0d: DerivativeRing(itertools.chain(xrange(steps), [steps - 1]), steps),
-        0x0b: DerivativeRing(itertools.chain(xrange(steps - 1, -1, -1), [steps - 1]), steps),
-        0x09: DerivativeRing(itertools.chain(xrange(steps - 1, -1, -1), [0]), steps),
-    }
-    for s in xrange(0x08):
-        shapes[s] = shapes[0x0f if s & 0x04 else 0x09]
-    del s
-
-    def __init__(self, scale, periodreg, shapereg):
-        scaleofstep = scale * 32 // self.steps
-        IntegralNode.__init__(self, SimpleDerivative(scaleofstep, periodreg, True))
-        self.shapeversion = None
-        self.shapereg = shapereg
-
-    def callimpl(self):
-        if self.shapeversion != self.shapereg.version:
-            self.derivative.reset(self.shapes[self.shapereg.value])
-            self.shapeversion = self.shapereg.version
-        IntegralNode.callimpl(self)
