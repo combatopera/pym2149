92168c2f40eabe38be9ad429f9e284b6582ebb38
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-03-15 21:40:40 +0000

    reindent

--- pym2149/program.py
+++ pym2149/program.py
@@ -23,159 +23,159 @@ log = logging.getLogger(__name__)
 
 class FX:
 
-  class SimpleValue:
+    class SimpleValue:
 
-    def __init__(self, value):
-      self.value = value
+        def __init__(self, value):
+            self.value = value
 
-    def set(self, value):
-      self.value = value
+        def set(self, value):
+            self.value = value
 
-    def step(self): pass
+        def step(self): pass
 
-  class SlideValue:
+    class SlideValue:
 
-    def __init__(self, slidespeed, value):
-      self.slidespeed = slidespeed
-      self.target = self.value = value
+        def __init__(self, slidespeed, value):
+            self.slidespeed = slidespeed
+            self.target = self.value = value
 
-    def set(self, value):
-      self.target = value & ~0x7f
-      self.rate = (value & 0x7f) * self.slidespeed
-      if not self.rate: # Simulate simple behaviour when fine part is 0.
-        self.value = self.target
+        def set(self, value):
+            self.target = value & ~0x7f
+            self.rate = (value & 0x7f) * self.slidespeed
+            if not self.rate: # Simulate simple behaviour when fine part is 0.
+                self.value = self.target
 
-    def step(self):
-      if self.value < self.target:
-        self.value = min(self.value + self.rate, self.target)
-      elif self.value > self.target:
-        self.value = max(self.value - self.rate, self.target)
+        def step(self):
+            if self.value < self.target:
+                self.value = min(self.value + self.rate, self.target)
+            elif self.value > self.target:
+                self.value = max(self.value - self.rate, self.target)
 
-  halfrange = 1 << 13 # Full range is 14 bits.
-  minsigned = -halfrange
-  maxsigned = halfrange - 1
+    halfrange = 1 << 13 # Full range is 14 bits.
+    minsigned = -halfrange
+    maxsigned = halfrange - 1
 
-  @staticmethod
-  def signum(n):
-    return 1 if n > 0 else (-1 if n < 0 else 0)
+    @staticmethod
+    def signum(n):
+        return 1 if n > 0 else (-1 if n < 0 else 0)
 
-  def __init__(self, config, slide):
-    self.bendpersemitone = config.pitchbendpersemitone
-    if slide:
-      slidespeed = config.slidespeed
-      valueimpl = lambda v: self.SlideValue(slidespeed, v)
-    else:
-      valueimpl = self.SimpleValue
-    self.bend = valueimpl(0)
-    self.modulation = valueimpl(self.halfrange)
-    self.pan = valueimpl(0)
+    def __init__(self, config, slide):
+        self.bendpersemitone = config.pitchbendpersemitone
+        if slide:
+            slidespeed = config.slidespeed
+            valueimpl = lambda v: self.SlideValue(slidespeed, v)
+        else:
+            valueimpl = self.SimpleValue
+        self.bend = valueimpl(0)
+        self.modulation = valueimpl(self.halfrange)
+        self.pan = valueimpl(0)
 
-  def applyrates(self):
-    for v in self.bend, self.modulation, self.pan:
-      v.step()
+    def applyrates(self):
+        for v in self.bend, self.modulation, self.pan:
+            v.step()
 
-  def bendsemitones(self):
-    return self.bend.value / self.bendpersemitone
+    def bendsemitones(self):
+        return self.bend.value / self.bendpersemitone
 
-  def relmodulation(self):
-    return (max(1, self.modulation.value) - self.halfrange) / (self.halfrange - 1) / 2 + .5
+    def relmodulation(self):
+        return (max(1, self.modulation.value) - self.halfrange) / (self.halfrange - 1) / 2 + .5
 
-  def normpan(self):
-    # Observe we don't apply maxpan, which is only for the auto-stereo:
-    return max(self.minsigned + 1, self.pan.value) / (self.halfrange - 1)
+    def normpan(self):
+        # Observe we don't apply maxpan, which is only for the auto-stereo:
+        return max(self.minsigned + 1, self.pan.value) / (self.halfrange - 1)
 
 class Note:
 
-  toneperiod = regproperty(lambda note: note.toneperiodreg)
-  toneflag = regproperty(lambda note: note.toneflagreg)
-  noiseflag = regproperty(lambda note: note.noiseflagreg)
-  fixedlevel = regproperty(lambda note: note.fixedlevelreg)
-  levelmode = regproperty(lambda note: note.levelmodereg)
-  tonepitch = regproperty(lambda note: note.tonepitchreg)
-  tonefreq = regproperty(lambda note: note.tonefreqreg)
+    toneperiod = regproperty(lambda note: note.toneperiodreg)
+    toneflag = regproperty(lambda note: note.toneflagreg)
+    noiseflag = regproperty(lambda note: note.noiseflagreg)
+    fixedlevel = regproperty(lambda note: note.fixedlevelreg)
+    levelmode = regproperty(lambda note: note.levelmodereg)
+    tonepitch = regproperty(lambda note: note.tonepitchreg)
+    tonefreq = regproperty(lambda note: note.tonefreqreg)
 
-  def __init__(self, nomclock, chip, chipchan, pitch, fx):
-    self.toneperiodreg = chip.toneperiods[chipchan]
-    self.toneflagreg = chip.toneflags[chipchan]
-    self.timer = chip.timers[chipchan]
-    self.noiseflagreg = chip.noiseflags[chipchan]
-    self.fixedlevelreg = Reg()
-    # No reverse link, we don't want to pollute the chip with references:
-    chip.fixedlevels[chipchan].link(lambda unclamped: max(0, min(15, unclamped)), self.fixedlevelreg)
-    self.levelmodereg = chip.levelmodes[chipchan]
-    self.nomclock = nomclock
-    self.chip = chip
-    self.chipchan = chipchan
-    self.pitch = pitch
-    self.fx = fx
-    self.tonefreqreg = Reg()
-    self.toneperiodreg.link(lambda f: f.toneperiod(nomclock), self.tonefreqreg) # No reverse link.
-    self.tonepitchreg = Reg()
-    self.tonefreqreg.link(lambda p: (p + fx.bendsemitones()).freq(), self.tonepitchreg)
+    def __init__(self, nomclock, chip, chipchan, pitch, fx):
+        self.toneperiodreg = chip.toneperiods[chipchan]
+        self.toneflagreg = chip.toneflags[chipchan]
+        self.timer = chip.timers[chipchan]
+        self.noiseflagreg = chip.noiseflags[chipchan]
+        self.fixedlevelreg = Reg()
+        # No reverse link, we don't want to pollute the chip with references:
+        chip.fixedlevels[chipchan].link(lambda unclamped: max(0, min(15, unclamped)), self.fixedlevelreg)
+        self.levelmodereg = chip.levelmodes[chipchan]
+        self.nomclock = nomclock
+        self.chip = chip
+        self.chipchan = chipchan
+        self.pitch = pitch
+        self.fx = fx
+        self.tonefreqreg = Reg()
+        self.toneperiodreg.link(lambda f: f.toneperiod(nomclock), self.tonefreqreg) # No reverse link.
+        self.tonepitchreg = Reg()
+        self.tonefreqreg.link(lambda p: (p + fx.bendsemitones()).freq(), self.tonepitchreg)
 
-  def _call(self, f, *args):
-    try:
-      f(*args)
-    except Exception:
-      log.exception("%s.%s failed:", type(self).__name__, f.__name__)
+    def _call(self, f, *args):
+        try:
+            f(*args)
+        except Exception:
+            log.exception("%s.%s failed:", type(self).__name__, f.__name__)
 
-  def callnoteon(self, voladj):
-    self.chip.flagsoff(self.chipchan) # Make it so that the impl only has to switch things on.
-    self.voladj = voladj
-    self._call(self.noteon)
+    def callnoteon(self, voladj):
+        self.chip.flagsoff(self.chipchan) # Make it so that the impl only has to switch things on.
+        self.voladj = voladj
+        self._call(self.noteon)
 
-  def noteon(self): pass
+    def noteon(self): pass
 
-  def callnoteonframe(self, *args):
-    self._call(self.noteonframe, *args)
+    def callnoteonframe(self, *args):
+        self._call(self.noteonframe, *args)
 
-  def noteonframe(self, frame):
-    """Note may never be called, so don't make changes that noteoff or a custom impl of noteoffframe later relies on."""
+    def noteonframe(self, frame):
+        """Note may never be called, so don't make changes that noteoff or a custom impl of noteoffframe later relies on."""
 
-  def callnoteoff(self, onframes):
-    self.onframes = onframes
-    self._call(self.noteoff)
+    def callnoteoff(self, onframes):
+        self.onframes = onframes
+        self._call(self.noteoff)
 
-  def noteoff(self): pass
+    def noteoff(self): pass
 
-  def callnoteoffframe(self, *args):
-    self._call(self.noteoffframe, *args)
+    def callnoteoffframe(self, *args):
+        self._call(self.noteoffframe, *args)
 
-  def noteoffframe(self, frame):
-    self.noteonframe(self.onframes + frame)
+    def noteoffframe(self, frame):
+        self.noteonframe(self.onframes + frame)
 
 class NullNote(Note): pass
 
 class DefaultNote(Note):
 
-  vib = FloatLFO(0).hold(10).tri(8, 2, .2).loop(8)
-  fadeout = LFO(-1).hold(1).lin(28, -15)
+    vib = FloatLFO(0).hold(10).tri(8, 2, .2).loop(8)
+    fadeout = LFO(-1).hold(1).lin(28, -15)
 
-  def noteon(self):
-    self.toneflag = True
-    self.fixedlevel = 13 + self.voladj
+    def noteon(self):
+        self.toneflag = True
+        self.fixedlevel = 13 + self.voladj
 
-  def noteonframe(self, frame):
-    self.tonepitch = self.pitch + self.fx.relmodulation() * self.vib(frame)
+    def noteonframe(self, frame):
+        self.tonepitch = self.pitch + self.fx.relmodulation() * self.vib(frame)
 
-  def noteoffframe(self, frame):
-    self.tonepitch = self.pitch + self.fx.relmodulation() * self.vib(self.onframes + frame)
-    self.fixedlevel = 13 + self.voladj + self.fadeout(frame)
+    def noteoffframe(self, frame):
+        self.tonepitch = self.pitch + self.fx.relmodulation() * self.vib(self.onframes + frame)
+        self.fixedlevel = 13 + self.voladj + self.fadeout(frame)
 
 class Unpitched(Note):
 
-  def __init__(self, *args):
-    super().__init__(*args)
-    self.note = self.midinotetoprogram.get(self.pitch, NullNote)(self.nomclock, self.chip, self.chipchan, None, self.fx)
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.note = self.midinotetoprogram.get(self.pitch, NullNote)(self.nomclock, self.chip, self.chipchan, None, self.fx)
 
-  def noteon(self):
-    self.note.callnoteon(self.voladj)
+    def noteon(self):
+        self.note.callnoteon(self.voladj)
 
-  def noteonframe(self, frame):
-    self.note.noteonframe(frame)
+    def noteonframe(self, frame):
+        self.note.noteonframe(frame)
 
-  def noteoff(self):
-    self.note.callnoteoff(self.onframes)
+    def noteoff(self):
+        self.note.callnoteoff(self.onframes)
 
-  def noteoffframe(self, frame):
-    self.note.noteoffframe(frame)
+    def noteoffframe(self, frame):
+        self.note.noteoffframe(frame)
