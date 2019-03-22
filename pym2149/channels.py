bdaa38735c063af3da7bd02c5b6daabda7abbf0b
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-03-22 19:58:33 +0000

    refactoring

--- pym2149/channels.py
+++ pym2149/channels.py
@@ -39,9 +39,10 @@ class NullChanNote:
 
 class ChanNote:
 
-    def __init__(self, onframe, program, note, midinote, fx):
+    def __init__(self, onframe, program, chip, note, midinote, fx):
         self.onframe = onframe
         self.program = program
+        self.chip = chip
         self.note = note
         self.midinote = midinote
         self.fx = fx
@@ -66,7 +67,7 @@ class ChanNote:
             self.update = lambda *args: None # Freeze this note.
 
     def _callnoteon(self):
-        self.note.chip.flagsoff(self.note.chipchan) # Make it so that the impl only has to switch things on.
+        self.chip.flagsoff(self.note.chipchan) # Make it so that the impl only has to switch things on.
         self.note.noteon()
 
     def _update(self, frame):
@@ -100,7 +101,7 @@ class Channel:
             return program.__name__
 
     def newnote(self, frame, program, midinote, vel, fx):
-        self.channote = ChanNote(frame, program, program(self.nomclock, self.chip, self.chipindex, Pitch(midinote), self.tovoladj(vel), fx), midinote, fx)
+        self.channote = ChanNote(frame, program, chip, program(self.nomclock, self.chip, self.chipindex, Pitch(midinote), self.tovoladj(vel), fx), midinote, fx)
 
     def noteoff(self, frame):
         self.channote.offframe = frame
