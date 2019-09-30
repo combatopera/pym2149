b439e6e7bb07b6dc984b775ef74bf695c239605a
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-09-30 21:19:02 +0100

    con

--- contrib/Per Almered - Hit me!.py
+++ contrib/Per Almered - Hit me!.py
@@ -37,7 +37,7 @@ class Arp:
         chip.tonepitch = chip.topitch((degree + inv[frame].pick(self.chords))[frame]) + self.vib[frame]
 
 bass = E(Bass, '1/.5 1/.5 .5 1/.5 1/.5 .5 1/.5 .5 1.5/1',
-        degree = D('---') + D('2 2+ .5x+ 2+ 1.5x2 2+ .5x+ 1.5x2+'))
+        degree = D('--') + D('2- 2 .5x 2 1.5x2- 2 .5x 1.5x2'))
 kick = E(Kick, '2')
 arp = E(Arp, '1.5 6.5',
         degree = D('1.5x 6.5x4'),
