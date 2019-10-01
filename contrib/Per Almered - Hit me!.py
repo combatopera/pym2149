0c7a90d359c9e61d5d6434e33217234469548ca2
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-10-01 20:24:26 +0100

    slapp

--- contrib/Per Almered - Hit me!.py
+++ contrib/Per Almered - Hit me!.py
@@ -4,9 +4,9 @@ from pym2149.pitches import F4
 class Bass:
 
     envshape = 0x0a
-    envpitch = D('2x++,+')
+    envpitches = D('2x++,+'), D('2x++ 5x+,1')
 
-    def on(self, frame, chip, degree, hard = V('0')):
+    def on(self, frame, chip, degree, slap = V('0'), hard = V('0')):
         chip.fixedlevel = 15
         chip.noiseflag = False
         chip.toneflag = True
@@ -14,7 +14,7 @@ class Bass:
         chip.envflag = True
         if frame < 1 and hard[frame]:
             chip.envshape = self.envshape
-        chip.envpitch = chip.topitch((self.envpitch + degree)[frame])
+        chip.envpitch = chip.topitch((slap[frame].pick(self.envpitches) + degree)[frame])
 
 class Kick:
 
@@ -59,21 +59,25 @@ class Arp:
         chip.toneflag = True
         chip.tonepitch = chip.topitch((degree + inv[frame].pick(self.chords))[frame]) + self.vib[frame]
 
-bass = E(Bass, 2 * ['/.5 /.5 .5 /.5 /.5 .5 /.5 .5 1.5/1|/.5 /.5 .5 /.5 /.5 .5 /.5 4x.5'],
+bass1 = E(Bass, 2 * ['/.5 /.5 .5 /.5 /.5 .5 /.5 .5 1.5/1|/.5 /.5 .5 /.5 /.5 .5 /.5 4x.5'],
         degree = D('--') + D('2- 2 .5x 2 1.5x2- 2 .5x 1.5x2|2- 2 .5x 2 1.5x2- 2 .5x .5x2 .5x6- .5x'),
         hard = V('1,0'))
 bass2 = E(Bass, '/.5 /.5 .5 /.5 /.5 .5 /.5 4x.5|/.5 /.5 .5 /.5 9x.5',
         degree = D('--') + D('2- 2 .5x 2 1.5x2- 2 .5x .5x2 .5x5-- .5x5-|6-- 6- .5x5- 6- 6-- .5x6- .5x7-- .5x7- .5x- .5x .5x#- .5x#'))
-kick = E(Kick, '2')
+bass3 = E(Bass, '32x.5',
+        degree = D('---') + D('2 2+ 2 2+ 2 2+ 2 2+ 2 2+ 3 3+ 4 4+ 5- 5|6- 6 6- 6 6- 6 6- 6 6- 6 7- 7 1 + # #+').of(.5),
+        slap = V('0 1 0 1 0 1 0 1 0 1 0 1 0 1 2x').of(.5))
+kick1 = E(Kick, '2')
 kick2 = E(Kick, '12x .5/.5 .25 .75 2.5')
 snare2 = E(Snare, '/13 1 .25 .5 .25 .5 2x.25',
         vel = V('14.25x1 .75x .5x1 .25x .25x1'))
 arp = E(Arp, '1.5 6.5',
         degree = D('1.5x 6.5x4 1.5x5 6.5x'),
         inv = V('1.5x2 6.5x1 1.5x 6.5x2'))
-A = bass, kick, arp
+A = bass1, kick1, arp
 B = bass2, kick2 & snare2, arp
-sections = A, B
+C = bass3,
+sections = A, B, C
 scale = major
 tonic = F4
 speed = 20
