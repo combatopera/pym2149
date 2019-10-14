3d27007362515729a3efde2bc18fef673e88c7e1
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-10-14 22:51:51 +0100

    porting samples to lc

--- pym2149/test_expected.py
+++ pym2149/test_expected.py
@@ -15,19 +15,35 @@
 # You should have received a copy of the GNU General Public License
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
+from .test_samples import batterypower
 from pathlib import Path
-import subprocess, unittest, sys
+import subprocess, unittest, sys, tempfile
 
 project = Path(__file__).parent.parent
 expected = project / 'expected'
+target = project / 'target'
+pngsuffix = '.png'
 
 def test_expected():
+    if batterypower():
+        return
     for path in expected.glob('**/*'):
         if not path.is_dir():
-            yield _compare, path
+            yield (_comparepng if path.name.endswith(pngsuffix) else _comparetxt), path
 
-def _compare(path):
-    command = [sys.executable, project / 'lc2txt.py', '--ignore-settings']
-    actual = subprocess.check_output(command + ["%s.py" % (project / path.relative_to(expected))]).decode()
+def _comparetxt(path):
+    actual = subprocess.check_output([sys.executable, project / 'lc2txt.py', '--ignore-settings',
+            "%s.py" % (project / path.relative_to(expected))], universal_newlines = True)
     with path.open() as f:
         unittest.TestCase().assertEqual(f.read(), actual)
+
+def _comparepng(path):
+    actualpath = target / path.relative_to(expected)
+    actualpath.parent.mkdir(parents = True, exist_ok = True)
+    with tempfile.NamedTemporaryFile() as wavfile:
+        subprocess.check_call([sys.executable, project / 'lc2wav.py', '--ignore-settings',
+                "%s.py" % str(project / path.relative_to(expected))[:-len(pngsuffix)], wavfile.name])
+        subprocess.check_call(['sox', wavfile.name, '-n', 'spectrogram', '-o', actualpath])
+    with path.open('rb') as f:
+        with actualpath.open('rb') as g:
+            unittest.TestCase().assertEqual(f.read(), g.read())
