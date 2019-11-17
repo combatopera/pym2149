52c6a37f6e90ff99c933b6dc43b98d642e12fac7
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-11-17 11:53:17 +0000

    retire Pitch

--- pym2149/pitches.py
+++ pym2149/pitches.py
@@ -16,7 +16,6 @@
 # along with pym2149.  If not, see <http://www.gnu.org/licenses/>.
 
 from .util import singleton
-from .pitch import Pitch
 
 @singleton
 class __all__(list):
@@ -25,5 +24,5 @@ class __all__(list):
     for octave in range(10):
       for letter, offset in zip('CDEFGAB', [0, 2, 4, 5, 7, 9, 11]):
         name = "%s%s" % (letter, octave)
-        globals()[name] = Pitch((1 + octave) * 12 + offset)
+        globals()[name] = (1 + octave) * 12 + offset
         self.append(name)
